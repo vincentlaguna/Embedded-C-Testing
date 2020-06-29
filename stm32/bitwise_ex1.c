c4b37686a41a9cbe3000f2ae8dce3a3ea79a9701
vincentlaguna <vincentlaguna@live.com>
2020-06-29 01:30:03 +0000

    bitwise 1 exercise - working commit

--- stm32/bitwise_ex1.c
+++ stm32/bitwise_ex1.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdint.h>
 
+void wait_for_user_input(void);
+
 int main(void)
 {
+  int32_t num1, num2;
+  printf("Enter 2 numbers (give space between the 2): \n");
+  scanf("%d %d", &num1, &num2);
+  
+  printf("Bitwise AND(&) -> [%d]\n", (num1 & num2));
+  printf("Bitwise OR(&) -> [%d]\n", (num1 | num2));
+  printf("Bitwise XOR(^) -> [%d]\n", (num1 ^ num2));
+  printf("Bitwise NOT(~) -> [%d]\n", (~num1));
+  
+  wait_for_user_input();
   return 0;
 }
+
+void wait_for_user_input(void)
+{
+  
+}
+
+
+
