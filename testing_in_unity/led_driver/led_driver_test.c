07d107a2530666b1c83980d72dacae53e2856afe
vincentlaguna <vincentlaguna@live.com>
2020-07-20 22:30:00 +0000

    led driver tests - commit #1 - C

--- testing_in_unity/led_driver/led_driver_test.c
+++ testing_in_unity/led_driver/led_driver_test.c
@@ -1,3 +1,18 @@
 #include "unity_fixture.h"
 
 TEST_GROUP(led_driver);
+
+TEST_SETUP(led_driver)
+{
+    
+}
+
+TEST_TEAR_DOWN
+{
+    
+}
+
+TEST(led_driver, ledsOffAfterCreate)
+{
+    TEST_FAIL_MESSAGE("Start here");
+}
