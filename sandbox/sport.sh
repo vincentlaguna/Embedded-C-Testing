173d9972c8de1636890fc5f11e088fa8beee3b23
vincentlaguna <vincentlaguna@live.com>
2020-07-29 22:00:51 +0000

    bash session practice #2

--- sandbox/sport.sh
+++ sandbox/sport.sh
@@ -2,8 +2,20 @@
 # First parameter to enter
 NAME=$1
 SPORT=$2
+COLOR=$3
 
 echo $NAME likes to watch $SPORT.
 
+if [ $COLOR = "blue" ]
+then
+    echo "The color is blue"
+fi
 
+USER_GUESS=$4
+COMPUTER=50
+
+if [ $USER_GUESS -lt $COMPUTER ]
+then
+    echo "You're too low"
+fi
 
