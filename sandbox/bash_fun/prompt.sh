0d57f8770a67a26b5d92d9ff4b9db524418ffc85
vincentlaguna <vincentlaguna@live.com>
2020-08-25 22:55:04 +0000

    getting input from the user and validating input - commit #1 - Bash

--- sandbox/bash_fun/prompt.sh
+++ sandbox/bash_fun/prompt.sh
@@ -3,4 +3,19 @@
 read -p "What is your first name: " NAME
 echo "Your name is: $NAME"
 
+VALID=0
+
+while [ $VALID -eq 0 ]
+do
+   read -p "Please enter name and age: " NAME AGE
+   if [[ ( -z $NAME ) || ( -z $AGE)]]
+   then
+      echo "Not enough parameters passed..."
+      continue
+   elif [[ ! $NAME =~ ^[A-Za-z]+$ ]] # Alphabetical validation through regex
+   then
+      echo "Non-alpha characters detected [$NAME]"
+      continue
+   elif [[ ! $NAME =~ ^[]+$ ]]
+done
 exit 0
