89032817065635ba8dd7e505797871a73856b122
vincentlaguna <vincentlaguna@live.com>
2020-08-27 21:24:01 +0000

    guessing game exercise - commit #2 - Bash

--- sandbox/bash_fun/guess.sh
+++ sandbox/bash_fun/guess.sh
@@ -8,7 +8,14 @@ do
    read -p "What is you guess? " INPUT
    if [ $INPUT -eq $COMPUTER ]
    then
-      
+      echo "You've won! The number was $COMPUTER"
+      exit 0
+   elif [ $INPUT -lt $COMPUTER ]
+   then
+      echo "The number was too low..."
+   else
+      echo "The number was too high..."
+   fi
 done
 
 exit 0
