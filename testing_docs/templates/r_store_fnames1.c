e80faebfbcaf4fe4d16c8af7163fec04db87bc9b
vincentlaguna <vincentlaguna@live.com>
2020-09-06 00:06:44 +0000

    read and store filenames - commit #2 - C

--- testing_docs/templates/r_store_fnames1.c
+++ testing_docs/templates/r_store_fnames1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
 
@@ -10,5 +11,22 @@ int main(void)
    FILE *output;
    struct dirent *entry;
    char path[SIZE];
+   const char filename[] = "filelist.txt";
+   int x, r;
+   // Open the output file (must do this first!) -> same directory
+   output = fopen(filename, "w");
+   if(output == NULL)
+   {
+      fprintf(stderr, "Unable to create file %s\n", filename);
+      exit(1);
+   }
+   // Fetch path for the directory to read
+   printf("Please enter the directory to be examined: ");
+   fgets(path, SIZE, stdin);
+   // Remove the newline
+   for(x = 0; x < SIZE; x++)
+   {
+      
+   }
    return(0);
 }
