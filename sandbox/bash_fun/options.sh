66930c8e14f2861d423ad113ff78276b99b36e3c
vincentlaguna <vincentlaguna@live.com>
2020-09-11 21:46:38 +0000

    case statement in bash - commit #1

--- sandbox/bash_fun/options.sh
+++ sandbox/bash_fun/options.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/bash
 # This is a basic bash script using the case statement
+while getopts u:p:ab option; do
+   case $option in
+      u) user=$OPTARG;;
+      p) user=$OPTARG;;
+   esac
+done
+
+echo "User: $user / Pass: $pass"
