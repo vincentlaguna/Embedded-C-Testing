823a71dbc5371e13060424c61c62716b8488c8d4
vincentlaguna <vincentlaguna@live.com>
2020-11-14 03:47:47 +0000

    UART for RPI - initial commit #0

---
+++ testing_docs/RPI_Files/RPI_UART.c
@@ -0,0 +1,37 @@
+// UART Communication on Raspberry Pi using C (WiringPi Library)
+// from http://www.electronicwings.com
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+int main(void)
+{
+  int serial_port;
+  char dat;
+  
+  if ((serial_port = serialOpen("/dev/ttyS0", 9600)) < 0) // Open serial port
+  {
+    fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
+    return(1);
+  }
+  
+  if (wiringPiSetup() == -1) // Initialize wiringPi Setup
+  {
+    fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
+    return(1);
+  }
+  
+  while (1)
+  {
+    if (serialDataAvail(serial_port))
+    {
+      dat = serialGetchar(serial_port); // Receive character serially
+      printf("%c", dat);
+      fflush(stdout);
+      serialPutchar(serial_port, dat); // Transmit the character serially on the port
+    }
+  }
+}
