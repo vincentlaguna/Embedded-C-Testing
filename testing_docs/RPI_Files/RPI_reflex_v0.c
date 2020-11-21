45f4625a02221f4886db75998f44d4d00dce8a20
vincentlaguna <vincentlaguna@live.com>
2020-11-21 21:49:24 +0000

    rpi reflex game stuff - commit #2

--- testing_docs/RPI_Files/RPI_reflex_v0.c
+++ testing_docs/RPI_Files/RPI_reflex_v0.c
@@ -1,5 +1,7 @@
 // Project for Reflex game on a RPi3-4 in C
 #include <stdio.h>
 #include <wiringPi.h>
-#define 
-int leds[]
+#define LED_NUM 6
+
+static int leds[6] = {5,12,17,22,0};
+static int switches[LED_NUM] = {6,13,19,23,24,0};
