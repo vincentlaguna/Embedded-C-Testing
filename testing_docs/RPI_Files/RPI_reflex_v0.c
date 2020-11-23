b086baf9802e2c1b85cf9564792d0b54044d2b70
vincentlaguna <vincentlaguna@live.com>
2020-11-23 22:01:53 +0000

    rpi reflex game stuff - commit #4

--- testing_docs/RPI_Files/RPI_reflex_v0.c
+++ testing_docs/RPI_Files/RPI_reflex_v0.c
@@ -3,5 +3,12 @@
 #include <wiringPi.h>
 #define LED_NUM 6
 // Define the arrays of elements that will correspond to our LED's and switches
+// Maybe a struct implementation may be a better option???
 static int leds[6] = {5,12,17,22,0};
 static int switches[LED_NUM] = {6,13,19,23,24,0};
+// Main function - need while loop somewhere?
+int main(void)
+{
+
+  return(0);
+}
