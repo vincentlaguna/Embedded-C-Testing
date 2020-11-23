03c85b7a6b9a36aa0566965b712a54b2a40078a5
vincentlaguna <vincentlaguna@live.com>
2020-11-23 22:12:27 +0000

    rpi reflex game stuff - commit #5

--- testing_docs/RPI_Files/RPI_reflex_v0.c
+++ testing_docs/RPI_Files/RPI_reflex_v0.c
@@ -1,14 +1,24 @@
 // Project for Reflex game on a RPi3-4 in C
 #include <stdio.h>
 #include <wiringPi.h>
-#define LED_NUM 6
+
+#define LED_NUM  6
+#define TRUE     1
+#define FALSE    0
+// Typedef for bool (didn't want to include yet another library...)
+typedef int bool
 // Define the arrays of elements that will correspond to our LED's and switches
 // Maybe a struct implementation may be a better option???
-static int leds[6] = {5,12,17,22,0};
-static int switches[LED_NUM] = {6,13,19,23,24,0};
+static int leds[6]           =  {5,12,17,22,0};
+static int switches[LED_NUM] =  {6,13,19,23,24,0};
 // Main function - need while loop somewhere?
 int main(void)
 {
-
+  // Let's define our variables
+  int rand_int         =  0;
+  bool button_valid    =  FALSE;
+  bool button_invalid  =  FALSE;
+  int points_max       =  10;
+  
   return(0);
 }
