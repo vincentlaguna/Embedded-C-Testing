04ce96029491cb1dcd0adf0bc7396995dedb951b
vincentlaguna <vincentlaguna@live.com>
2020-11-23 21:59:38 +0000

    rpi reflex game stuff - commit #3

--- testing_docs/RPI_Files/RPI_reflex_v0.c
+++ testing_docs/RPI_Files/RPI_reflex_v0.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 #include <wiringPi.h>
 #define LED_NUM 6
-
+// Define the arrays of elements that will correspond to our LED's and switches
 static int leds[6] = {5,12,17,22,0};
 static int switches[LED_NUM] = {6,13,19,23,24,0};
