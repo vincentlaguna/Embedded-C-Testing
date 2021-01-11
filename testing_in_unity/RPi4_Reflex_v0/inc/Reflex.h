d269c6d0b68a2513eddf78f381d769011f9bfd70
vincentlaguna <vincentlaguna@live.com>
2021-01-11 23:19:49 +0000

    RPi4 Reflex v.0 - commit #3

---
+++ testing_in_unity/RPi4_Reflex_v0/inc/Reflex.h
@@ -0,0 +1,11 @@
+#ifndef REFLEX_H
+#define REFLEX_H
+
+typedef struct Buttons
+{
+  int button1;   
+};
+
+int init_Buttons(void);
+
+#endif //REFLEX_H
---
+++ testing_in_unity/RPi4_Reflex_v0/src/Reflex_Main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../inc/Reflex.h"
+#define _TEST
+
+#ifdef _TEST
+int Tenmw(void)
+{
+#endif
+
+#ifndef _TEST
+int main(void)
+{  
+#endif
+
+    printf("<<< Reflex Test Game >>>\n\n");
+    
+    return(0);
+}
