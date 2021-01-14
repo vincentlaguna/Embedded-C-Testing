837b272d0c383f24c4430c6a9277fad8d74225ab
vincentlaguna <vincentlaguna@live.com>
2021-01-14 20:53:13 +0000

    RPi4 Reflex v.0 - using bit fields to pack button data attempt - commit #11

--- testing_in_unity/RPi4_Reflex_v0/inc/Reflex.h
+++ testing_in_unity/RPi4_Reflex_v0/inc/Reflex.h
@@ -12,18 +12,18 @@ typedef struct
   uint8_t LED_4 :1;
   uint8_t LED_5 :1;
   uint8_t LED_6 :1;
-  uint8_t         :2;
+  uint8_t       :2;
 }LEDS;
 
 typedef struct
 {
-  uint8_t button1 :1;
-  uint8_t button2 :1;
-  uint8_t button3 :1;
-  uint8_t button4 :1;
-  uint8_t button5 :1;
-  uint8_t button6 :1;
-  uint8_t         :2;
+  uint8_t button_1 :1;
+  uint8_t button_2 :1;
+  uint8_t button_3 :1;
+  uint8_t button_4 :1;
+  uint8_t button_5 :1;
+  uint8_t button_6 :1;
+  uint8_t          :2;
 }Buttons;
 
 int Buttons_Init(void);
