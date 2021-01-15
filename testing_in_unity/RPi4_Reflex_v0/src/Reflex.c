7da8f46f12fc088d53f3cfb3812d7c152f196cbf
vincentlaguna <vincentlaguna@live.com>
2021-01-15 22:59:03 +0000

    RPi4 Reflex v.0 - using bit fields to pack button data attempt - LED struct implementation - commit #16

--- testing_in_unity/RPi4_Reflex_v0/src/Reflex.c
+++ testing_in_unity/RPi4_Reflex_v0/src/Reflex.c
@@ -17,8 +17,8 @@ int Buttons_Init(void)
   button->button_5 |= ON;
   button->button_6 |= ON;
   
-  if (button->button_1 && button->button_2 && button->button_3
-      && button->button_1 && button->button_1 &&)
+  if (button->button_1 && button->button_2 && button->button_3 &&
+      button->button_4 && button->button_5 && button->button_6)
     return 1;
   return 0;
 }
@@ -32,7 +32,14 @@ int LEDS_Init(void)
 //   pinMode(ButtonPin, INPUT);
 //   pullUpDnControl(ButtonPin, PUD_UP); //pull up to 3.3V,make GPIO1 a stable level
   LED->LED_1 |= ON;
-  if (LED->LED_1)
+  LED->LED_2 |= ON;
+  LED->LED_3 |= ON;
+  LED->LED_4 |= ON;
+  LED->LED_5 |= ON;
+  LED->LED_6 |= ON;
+  
+  if (LED->LED_1 && LED->LED_2 && LED->LED_3 &&
+      LED->LED_4 && LED->LED_5 && LED->LED_6)
     return 1;
   return 0;
 }
