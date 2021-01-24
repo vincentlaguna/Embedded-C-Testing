de9e4231679230cf3bad89880b5a9bc67ca88837
vincentlaguna <vincentlaguna@live.com>
2021-01-24 01:47:19 +0000

    IPC Review - pthreads - commit #1 - C

--- sandbox/IPC/pthreads_0.c
+++ sandbox/IPC/pthreads_0.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <pthread.h>
 
+#define NTHREADS 10
+
+int counter = 0;
+
+int main(void)
+{
+  pthread_t thread_id[NTHREADS]; // Create array of 10 threads
+  int values[NTHREADS];
+  
+  for (int i = 0; i < NTHREADS; i++)
+  {
+    values[i] = i;
+    pthread__create(&thread[i], NULL, function_1, &values[i]);
+  }
+  
+  return(0);
+}
