d96672b53425d257defcc715c992434a3468b476
vincentlaguna <vincentlaguna@live.com>
2021-01-24 19:35:02 +0000

    IPC Review - Socket API - commit #1 - C

--- sandbox/IPC/Socket_API_Review_0.c
+++ sandbox/IPC/Socket_API_Review_0.c
@@ -2,8 +2,13 @@
 // Struct sockaddr definition:
 struct sockaddr
 {
-  unsigned short sa_family; // AF_INET (most used), AF_UNIX, AF_NS, AF_IMPLINK
-  char           sa_data[14]; // Protocol-specific address
+  unsigned short        sa_family; // AF_INET (most used), AF_UNIX, AF_NS, AF_IMPLINK
+  char                  sa_data[14]; // Protocol-specific address
+};
+
+struct in_addr // Only used as a structure field; holds 32-bit netid/hostid
+{
+  unsigned long         s_addr;
 };
 
 struct sockaddr_in
@@ -12,4 +17,15 @@ struct sockaddr_in
   unsigned short int    sin_port;
   struct in_addr        sin_addr;
   unsigned char         sin_zero[8];
+};
+
+struct hostent
+{
+  char                  *h_name;
+  char                  **h_aliases;
+  int                   h_addrtype;
+  int                   h_length;
+  char                  **h_addr_list
+  
+#define h_addr h_addr_list[0]
 };
