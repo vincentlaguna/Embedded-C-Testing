30a402e8dd8d6d020eb05776377a2d647287014e
vincentlaguna <vincentlaguna@live.com>
2021-01-24 05:04:26 +0000

    IPC Review - pthreads - commit #5 - C

--- sandbox/IPC/pthreads_0.c
+++ sandbox/IPC/pthreads_0.c
@@ -4,6 +4,9 @@
 #define NTHREADS 10
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t evens_done = PTHREAD_COND_INITIALIZER;
+
+int number_evens_finished = 0;
 
 int counter = 0; // Global variable to be manipulated by our threads
 
@@ -18,14 +21,25 @@ int main(void)
   {
     for (int i = 0; i < NTHREADS; i++) // For-Loop to create the 10 threads
     {
-        values[i] = i;
-        pthread_create(&thread_id[i], NULL, function_1, &values[i]);
+      values[i] = i;
+      pthread_create(&thread_id[i], NULL, function_1, &values[i]);
     }
-  
+    
+    sleep(1);
+    
+    while(1)
+    {
+      if (number_evens_finished == NTHREADS / 2)
+      {    
+        pthread_cond_broadcast(&evens_done);
+        break;
+      }
+    }
+    
     for (int j = 0; j < NTHREADS; j++)
     {
-        pthread_join(thread_id[j], NULL); // Join the 10 threads to Main
-        //pthread_exit(&thread_id[j]); // Kill Threads before exiting the program
+      pthread_join(thread_id[j], NULL); // Join the 10 threads to Main
+      //pthread_exit(&thread_id[j]); // Kill Threads before exiting the program
     }
     counter = 0;
   }
@@ -37,6 +51,15 @@ void *function_1(void *data) // Function to pass to pthread_create
   int *x = (int *)data; // Type-cast to *int, as the prototype
                         // only allows for a void *
   pthread_mutex_lock(&lock);
+  
+  if (*x % 2 == 0)
+  {
+    number_evens_finished++;
+  }
+  else
+  {
+    pthread_cond_wait(&evens_done, &lock); 
+  }
   counter++;
   
   printf("Message is %d, thread_id -> %lud modified the counter to = %d\n", *x, pthread_self(), counter);
