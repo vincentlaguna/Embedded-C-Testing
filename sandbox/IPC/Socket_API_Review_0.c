998ddce89644c70c4edaed89a29949b9668bb9fb
vincentlaguna <vincentlaguna@live.com>
2021-01-24 19:52:37 +0000

    IPC Review - Socket API - commit #2 - C

--- sandbox/IPC/Socket_API_Review_0.c
+++ sandbox/IPC/Socket_API_Review_0.c
@@ -19,13 +19,27 @@ struct sockaddr_in
   unsigned char         sin_zero[8];
 };
 
-struct hostent
+struct hostent // Keeps information relating to a Server
 {
-  char                  *h_name;
-  char                  **h_aliases;
+  char                 *h_name;
+  char                **h_aliases;
   int                   h_addrtype;
   int                   h_length;
-  char                  **h_addr_list
+  char                **h_addr_list;
   
 #define h_addr h_addr_list[0]
 };
+
+// Steps in using sockets to communicate:
+
+// 1. create a new socket for network communication ("socket") - Open socket
+// 2. Attach a local address to a socket ("bind") - Server Only
+// 3. Announce willingness to connect ("listen") - On Server
+// 4. Block caller until a connection request arrives ("accept") - 
+// 5. Actively attempt to establish a connection request ("connect") - Client initiates this step
+// 6. Send some data over connection ("send")
+// 7. Receive some data over connection ("receive")
+// 8. Release the connection ("close")
+
+// Remember: Socket, Bind, Listen, Accept, Connect, Send, Receive, Close
+
