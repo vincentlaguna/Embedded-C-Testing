85d5d256e66017926e03aa6f1632bdeeb236ec60
vincentlaguna <vincentlaguna@live.com>
2021-01-25 22:23:15 +0000

    IPC Review - Socket API - commit #10 - C

--- sandbox/IPC/Client-Server/server_0.c
+++ sandbox/IPC/Client-Server/server_0.c
@@ -6,11 +6,30 @@
 
 // start loop
 // {
-//     their_socket_descriptor = accept(my_socket_descriptor,
-//                                      <empty address to be filled with
-//                                      their incoming info>)
-//     recv(their_socket_descriptor, <where to put what you receive>)
-//     send(their_socket_descriptor, <the stuff you want to send>)
-//     close(my_socket_descriptor)
+//   their_socket_descriptor = accept(my_socket_descriptor,
+//                                    <empty address to be filled with
+//                                    their incoming info>)
+//   recv(their_socket_descriptor, <where to put what you receive>)
+//   send(their_socket_descriptor, <the stuff you want to send>)
+//   close(my_socket_descriptor)
 // }
 // end loop
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+short createSocket(void)
+{
+  short servSocket;
+  printf("\n<<< This is a simplified example of creating a Server-side "
+         "socket for a TCP/IP connection >>>\n");
+}
+
+// int main(void)
+// {
+//     printf("\n<<< This is a simplified example of creating a Server-side "
+//          "socket for TCP/IP connection >>>\n");
+//     return(0);
+// }
