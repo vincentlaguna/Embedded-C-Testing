04ebdca967c5253ef15fb6ddf1bb8a2babb43781
vincentlaguna <vincentlaguna@live.com>
2021-02-09 23:27:25 +0000

    Client-Server Review, Client-Side (second client) implementation- commit #0 -C

--- sandbox/IPC/Client-Server/client_1.c
+++ sandbox/IPC/Client-Server/client_1.c
@@ -5,3 +5,20 @@
 // send(their_socket_descriptor, <stuff you want to send>)
 // recv(their_socket_descriptor, <where to put what you receive>)
 // close(my_sd)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#define TEST_PORT 12345
+#ifdef _LOCAL_TEST
+  #define LOCAL "127.0.0.1"
+#endif
+// Helper Functions
+// Create a Socket for Server Communication
+short createSocket(void)
+{
+  
+}
