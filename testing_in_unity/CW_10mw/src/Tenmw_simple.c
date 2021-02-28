f09b1a8b501d36d548f9cfea62df626d53860c2e
vincentlaguna <vincentlaguna@live.com>
2021-02-28 05:13:24 +0000

    Client-Server implementation for network testing - try to output client messages to a file... - commit #11

--- testing_in_unity/CW_10mw/src/Tenmw_simple.c
+++ testing_in_unity/CW_10mw/src/Tenmw_simple.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define SQUARE_DIMENSION 11
+#define  SQUARE_DIMENSION 11
 
 char gridSquare[SQUARE_DIMENSION][SQUARE_DIMENSION] =
   {
