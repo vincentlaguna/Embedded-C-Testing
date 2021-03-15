a5f9d38c2993b879f30b62f00c1a514dd78f14c6
vincentlaguna <vincentlaguna@live.com>
2021-03-15 21:27:13 +0000

    Going for a single-function implementation on 10 min. walk - commit #2

--- testing_in_unity/CW_10mw/src/Tenmw_f.c
+++ testing_in_unity/CW_10mw/src/Tenmw_f.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
 #define SQUARE_DIMENSION 22
 
 int main(void)
@@ -24,6 +25,7 @@ int main(void)
   char marker = 'X';
   int count = 0;
   bool isValidWalkLength = false;
+  bool isBackHome = false;
   // Check for valid string length on *walk
   while (*walk)
   {
@@ -39,5 +41,50 @@ int main(void)
   {
     printf("Walk is not of valid length -> %d\n", count);
   }
+  // Move marker across 2D grid
+  pGrid = gridSquare;
+  
+  for (; *walk != '\0'; *walk++)
+  {
+    switch (*walk)
+    {
+      case 'n':
+        //marker = *(grid + (row-- * n) + position);
+        *(pGrid + (row-- * SQUARE_DIMENSION) + position) = marker;
+        break;
+      case 's': 
+        //marker = *(grid + (row-- * n) + position);
+        *(pGrid + (row++ * SQUARE_DIMENSION) + position) = marker;
+        break;
+      case 'e':
+        //marker = *(grid + (row-- * n) + position);
+        *(pGrid + (row * SQUARE_DIMENSION) + position--) = marker;
+        break;
+      case 'w':
+        //marker = *(grid + (row-- * n) + position);
+        *(pGrid + (row * SQUARE_DIMENSION) + position++) = marker;
+        break;
+    }
+    for (int x = 0; x < SQUARE_DIMENSION; x++)
+    {
+      for (int y = 0; y < SQUARE_DIMENSION; y++)
+      {
+        printf("%c ", *(pGrid + ((x * SQUARE_DIMENSION) + y)));
+      }
+    printf("\n");
+    }
+  printf("\n");
+  sleep(1);
+  }
+  if (marker == *(pGrid + (row * SQUARE_DIMENSION) + position))
+  {
+    isBackHome = true;
+    printf("Walker is back home!\n");
+  }
+  else
+  {
+    printf("Walker did not make it :(\n");
+  }
+    
   return(0);
 }
