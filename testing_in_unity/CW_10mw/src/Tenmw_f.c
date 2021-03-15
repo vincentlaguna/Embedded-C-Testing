04af8e693100e5d03ba20542e7c97953d92a2fc5
vincentlaguna <vincentlaguna@live.com>
2021-03-15 21:03:07 +0000

    Going for a single-function implementation on 10 min. walk - commit #0

--- testing_in_unity/CW_10mw/src/Tenmw_f.c
+++ testing_in_unity/CW_10mw/src/Tenmw_f.c
@@ -2,7 +2,25 @@
 #include <stdbool.h>
 #define SQUARE_DIMENSION 22
 
-int Tenmw_f(void)
+int main(void)
 {
+  char *walk = {"ewewewewew"}; // Pass
+  //char *walk = {"nnwwwsseee"}; // Pass
+  //char *walk = {"nsnsnsnsns"}; // Pass
+  //char *walk = {"nsnwensse"}; // Not Valid Length and Move
+  //char *walk = {"nnwwwssees"}; // Not Valid Move
+  //char *walk = {"eeeewnwwww"}; // Not Valid Move
+  //char *walk = {"wwwwwwwwww"}; // Not Valid Move
+  //char *walk = {"nnnnnnnnnn"}; // Not Valid Move
+  //char *walk = {"ssssssssss"}; // Not Valid Move
+  //char *walk = {"eeeeeeeeee"}; // Not Valid Move
+  char gridSquare[SQUARE_DIMENSION][SQUARE_DIMENSION];
+  char *pGrid = NULL;
+  int row = 11;
+  int position = 11;
+  char marker = 'X';
+  int count = 0;
+  bool isValidWalkLength = false;
+  
   return(0);
 }
