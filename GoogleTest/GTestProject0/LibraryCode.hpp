c3b2bd15e80b2d3942b5bf0d2a2d4bcce422dee3
vincentlaguna <vincentlaguna@live.com>
2021-05-25 04:35:15 +0000

    Account Class implementation - commit #0 - Google Test C++

--- GoogleTest/GTestProject0/LibraryCode.hpp
+++ GoogleTest/GTestProject0/LibraryCode.hpp
@@ -1,11 +1,24 @@
 #pragma once
-#include <vector>
-// Simple Function Definition
-int add(int a, int b);
-
-double mySqrt(double x);
-
-int countPositives(std::vector<int> const &inputVector);
-
-void toUpper(char *inputString);
+// #include <vector>
 // Move unto implementing classes here or new lib.hpp
+
+class Account
+{
+  public
+    Account();
+    void deposit(double amount);
+    void withdraw(double amount);
+    double getBalance() const;
+    void transfer(Account &to, double amount);
+  private
+    double mBalance;
+};
+
+// Simple Function Definition
+// int add(int a, int b);
+
+// double mySqrt(double x);
+
+// int countPositives(std::vector<int> const &inputVector);
+
+// void toUpper(char *inputString);
