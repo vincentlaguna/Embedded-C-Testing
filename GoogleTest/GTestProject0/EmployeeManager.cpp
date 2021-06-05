511bc46538232590a161ed004144bf26cd08ca58
vincentlaguna <vincentlaguna@live.com>
2021-06-05 00:33:18 +0000

    Employee Manager Project Implementation - Mocked Database Connection Class testRunner.cpp - First Passing Test (staying at this until errors are eliminated in the following test involving the getter functions - commit #14 - Google Test C++

--- GoogleTest/GTestProject0/EmployeeManager.cpp
+++ GoogleTest/GTestProject0/EmployeeManager.cpp
@@ -12,32 +12,32 @@ EmployeeManager::~EmployeeManager()
   mDbConnection->disconnect();
 }
 
-// float EmployeeManager::getSalary(int employeeId) const
-// {
-//   return mDbConnection->getSalary(employeeId);
-// }
+float EmployeeManager::getSalary(int employeeId) const
+{
+  return mDbConnection->getSalary(employeeId);
+}
 
-// void EmployeeManager::setSalary(int employeeId, float newSalary)
-// {
-//   mDbConnection->updateSalary(employeeId, newSalary);
-// }
+void EmployeeManager::setSalary(int employeeId, float newSalary)
+{
+  mDbConnection->updateSalary(employeeId, newSalary);
+}
 
-// std::map<std::string, float> EmployeeManager::vectorToMap(std::vector<Employee> const &employees) const
-// {
-//   std::map<std::string, float> result;
-//   for (Employee const &employee : employees)
-//   {
-//     result[employee.name] = employee.salary;
-//   }
-//   return result;
-// }
+std::map<std::string, float> EmployeeManager::vectorToMap(std::vector<Employee> const &employees) const
+{
+  std::map<std::string, float> result;
+  for (Employee const &employee : employees)
+  {
+    result[employee.name] = employee.salary;
+  }
+  return result;
+}
 
-// std::map<std::string, float> EmployeeManager::getSalariesGreater(float low) const
-// {
-//   return vectorToMap(mDbConnection->getSalariesRange(low));
-// }
+std::map<std::string, float> EmployeeManager::getSalariesGreater(float low) const
+{
+  return vectorToMap(mDbConnection->getSalariesRange(low));
+}
 
-// std::map<std::string, float> EmployeeManager::getSalariesBetween(float low, float high) const
-// {
-//   return vectorToMap(mDbConnection->getSalariesRange(low, high));
-// }
+std::map<std::string, float> EmployeeManager::getSalariesBetween(float low, float high) const
+{
+  return vectorToMap(mDbConnection->getSalariesRange(low, high));
+}
