d04fd7235472b944373960160513a960043ed755
vincentlaguna <vincentlaguna@live.com>
2021-07-01 20:59:28 +0000

    CW10mw: Random Character Generator Function - implementation - commit #4 - C

--- testing_in_unity/CW_10mw/test/TestTenmw.c
+++ testing_in_unity/CW_10mw/test/TestTenmw.c
@@ -18,7 +18,7 @@ void test_fWalk_should_notBeTenMinutes(void)
 
 void test_moveWalkerHome_should_return_True_onValidPosition(void)
 {
-  char *pGrid = NULL;
+  void *pGrid = NULL;
   pGrid = gridSquare;
   
   char *walk = {"nnwwwsseee"};
@@ -29,7 +29,7 @@ void test_moveWalkerHome_should_return_True_onValidPosition(void)
 
 void test_moveWalkerHome_should_return_False_onInvalidPosition(void)
 {
-  char *pGrid = NULL;
+  void *pGrid = NULL;
   pGrid = gridSquare;
   
   char *walk = {"nsnwensses"};
@@ -37,29 +37,30 @@ void test_moveWalkerHome_should_return_False_onInvalidPosition(void)
   TEST_ASSERT_FALSE(moveWalkerHome(walk, pGrid, SQUARE_DIMENSION));
 }
 
-void test_moveWalkerOutofBounds_should_return_False(void)
-{
-  char *pGrid = NULL;
-  pGrid = gridSquare;
-  
-  const char *walk = {"eeeeeeeeee"};
-  
-  TEST_ASSERT_FALSE(moveWalkerHome(walk, pGrid, SQUARE_DIMENSION));
-}
-
-// void test_moveWalkerHome_should_return_True_onValidPosition(void)
+// void test_moveWalkerOutofBounds_should_return_False(void)
 // {
-//   char *pGrid = NULL;
+//   void *pGrid = NULL;
 //   pGrid = gridSquare;
   
-//   const char *walk = {"nnwwwsseen"};
-//   //const char *walk = {"wwwwwwwwww"};
+//   const char *walk = {"eeeeeeeeee"};
   
 //   TEST_ASSERT_FALSE(moveWalkerHome(walk, pGrid, SQUARE_DIMENSION));
 // }
 
+// void test_moveWalkerHome_should_return_True_onValidPositionEveryTime(void)
+// {
+//   randomCharGenerator();
+//   // char *pGrid = NULL;
+//   // pGrid = gridSquare;
+  
+//   // const char *walk = {"nnwwwsseen"};
+//   // //const char *walk = {"wwwwwwwwww"};
+  
+//   // TEST_ASSERT_TRUE(moveWalkerHome(walk, pGrid, SQUARE_DIMENSION));
+// }
+
 // void test_TheFifth(void) {
-//     TEST_ASSERT_FALSE(1 == 1);
+//     TEST_ASSERT_TRUE(0 == 1);
 // }
 
 int main(void) 
@@ -70,7 +71,9 @@ int main(void)
   RUN_TEST(test_fWalk_should_notBeTenMinutes);
   RUN_TEST(test_moveWalkerHome_should_return_True_onValidPosition);
   RUN_TEST(test_moveWalkerHome_should_return_False_onInvalidPosition);
-  RUN_TEST(test_moveWalkerOutofBounds_should_return_False);
+  // RUN_TEST(test_moveWalkerOutofBounds_should_return_False);
+  // RUN_TEST(test_moveWalkerHome_should_return_True_onValidPositionEveryTime);
   // RUN_TEST(test_TheFifth);
+  
   return UNITY_END();
 }
