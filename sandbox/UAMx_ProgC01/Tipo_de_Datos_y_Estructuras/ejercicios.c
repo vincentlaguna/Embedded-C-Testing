1bee44edfe241bf44ff55a713159aabfd2c0a7f6
vincentlaguna <vincentlaguna@live.com>
2021-07-10 00:41:39 +0000

    UAMx ProgC01 - EDx Course - Conversion de Tipos - cometida #0 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -352,7 +352,7 @@ int main(void)
 /*********************************************/
 
 
-/*************** Ejercicio 2.6 ***************/
+/*************** Ejercicio 2.6 ***************
 // Y por último, vamos a modificar el programa anterior para que nos permita 
 // descifrar cinco letras que introduzca el usuario, de una en una.
 // Este sería un ejemplo de entrada y salida del programa, utilizando 
@@ -394,7 +394,33 @@ int main(void)
 
 /*********************************************/  
 
+
 /*********************************************/  
+// Conversion de tipos
+
+#include <stdio.h>
+
+int main() {
+    char c = 'c';
+    short s = c;
+    int i = s;
+    long l = i;
+    float f = l;
+    double d = f;
+    printf("char: %c; short: %hd; int: %d; long: %ld; float: %f; double: %lf\n", 
+            c, s, i, l, f, d);   
+   
+    d = 123.45678987654321e+23;
+    f = (float) d;
+    l = (long) f;
+    i = (int) l;
+    s = (short) i;
+    c = (char) s;
+    printf("double: %lf; float: %f; long: %ld; int: %d; short: %hd; char: %c\n", 
+            d, f, l, i, s, c);
+
+    return 0;
+}
 
 /*********************************************/  
 
