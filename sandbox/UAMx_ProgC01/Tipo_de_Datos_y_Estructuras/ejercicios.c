1f3bf51378f56aebd9ee16df08bd277cfcd37571
vincentlaguna <vincentlaguna@live.com>
2021-07-10 00:51:55 +0000

    UAMx ProgC01 - EDx Course - Conversion de Tipos - cometida #2 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -401,23 +401,29 @@ int main(void)
 #include <stdio.h>
 
 int main() {
-    char c = 'c';
-    short s = c;
-    int i = s;
-    long l = i;
-    float f = l;
-    double d = f;
-    printf("char: %c; short: %hd; int: %d; long: %ld; float: %f; double: %lf\n", 
-            c, s, i, l, f, d);   
+  char c = 'c';
+  short s = c;
+  int i = s;
+  long l = i;
+  float f = l;
+  double d = f;
+  printf("char: %c; short: %hd; int: %d; long: %ld; float: %f; double: %lf\n", 
+          c, s, i, l, f, d);   
    
-    d = 123.45678987654321e+23;
-    f = (float) d;
-    l = (long) f;
-    i = (int) l;
-    s = (short) i;
-    c = (char) s;
-    printf("double: %lf; float: %f; long: %ld; int: %d; short: %hd; char: %c\n", 
-            d, f, l, i, s, c);
+  d = 123.45678987654321e+23;
+  f = (float) d;
+  l = (long) f;
+  i = (int) l;
+  s = (short) i;
+  c = (char) s;
+  printf("double: %lf; float: %f; long: %ld; int: %d; short: %hd; char: %c\n", 
+          d, f, l, i, s, c);
+  
+  double x;
+  // v 5.0 / 2 = correct value assigned (2 gets promoted to double)
+  x = 5 / 2; // Incorrect value gets assigned as a double (5 nor 2 get promoted)
+  printf("%lf\n", x);          
+    
 
     return 0;
 }
