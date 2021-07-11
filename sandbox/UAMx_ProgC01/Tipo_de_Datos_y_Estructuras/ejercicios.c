9133c84619dab009e77e33d4a7fae17bcb9695b7
vincentlaguna <vincentlaguna@live.com>
2021-07-11 02:10:08 +0000

    UAMx ProgC01 - EDx Course - 3 Tablas y Cadenas - cometida #0 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -477,7 +477,7 @@ int main(void)
 
 
 
-/**************** Prueba 2.2 ****************/
+/**************** Prueba 2.2 ****************
 // Escribe un programa que solicite uno a uno dos caracteres.
 // El programa debe sumar el código ASCII de ambos caracteres y mostrar 
 // el resultado, incluyendo el código ASCII de cada uno de ellos.
@@ -510,8 +510,29 @@ int main(void)
 }
 
 /********************************************/  
+// Tablas y Cadenas - Macros y Constantes
 
+#include <stdio.h>
 
+#define PIES_POR_CM 0.03
+
+int main(void) 
+{
+  int ancho, largo;
+  double ancho_p, largo_p;
+
+  printf ("Introduce el ancho: ");
+  scanf ("%d", &ancho);
+  ancho_p = ancho * PIES_POR_CM;
+   
+  printf ("Introduce el largo: ");
+  scanf ("%d", &largo);
+  largo_p = largo * PIES_POR_CM;
+
+  printf ("%d x %d cm son %.2lf x %.2lf pies.\n", ancho, largo, ancho_p, largo_p);
+
+  return 0;
+}
 
 /*********************************************/  
 
