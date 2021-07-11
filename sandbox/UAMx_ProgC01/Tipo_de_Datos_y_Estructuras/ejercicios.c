17881560b4224b5f008318a9c42f716da5f3cf2a
vincentlaguna <vincentlaguna@live.com>
2021-07-11 22:30:46 +0000

    UAMx ProgC01 - EDx Course - 3 Tablas y Cadenas - Ejercicio 3.1 - cometida #3 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -509,6 +509,9 @@ int main(void)
   return 0;
 }
 
+/********************************************/  
+
+
 /********************************************
 // Tablas y Cadenas - Macros y Constantes
 
@@ -597,14 +600,72 @@ int main(void)
 /*********************************************/  
 
 
+/**************** Prueba 3.1 ****************
+// Escribe un programa en C que declare:
+
+// Una tabla de seis enteros (utiliza una macro para el tamaño).
+// Un entero para almacenar el resto de una división.
+// Dos variables de tipo float a double para calcular unos valores medios.
+// A continuación solicitará seis números enteros y los almacenará 
+// en cada una de las posiciones de la tabla.
+
+// Seguidamente calculará la media de las posiciones pares de la tabla 
+// (incluida la posición cero) y la guardará en una variable.
+
+// A continuación calculará la media de las posiciones impares 
+// de la tabla y la guardará en otra variable.
+
+// Por último, calculará el resto de la división de las 
+// primeras y última posiciones de la tabla.
+
+// Antes de terminar mostrará por pantalla tres mensajes 
+// con toda la información calculada.
+
+// Los mensajes se han de mostrar tal y como aparecen en 
+// el siguiente ejemplo de ejecución y en el mismo orden 
+// (variando solo según los datos introducidos por el usuario).
+
+// Lo que sigue es un ejemplo de ejecución del programa:
+
+// Introduce el primer número entero: 3
+// Introduce el segundo número entero: 312
+// Introduce el tercer número entero: -45
+// Introduce el cuarto número entero: 129
+// Introduce el quinto número entero: 81
+// Introduce el sexto número entero: 17
+// La media de los números 3, -45 y 81 es 13.000000
+// La media de los números 312, 129 y 17 es 152.666667
+// El resto de la divisón de 17 y 3 es 2
+
+#include <stdio.h>
+
+int main(void) 
+{
+  char caracter1, caracter2;
+    
+  printf("\nPor favor, introduce un carácter: ");
+  scanf("\n%c", &caracter1);
+  printf("Por favor, introduce otro carácter: ");
+  scanf("\n%c", &caracter2);
+  
+  printf("\nSi sumamos el código ASCII del carácter %c que es %d con el código "
+         "ASCII del carácter %c que es %d obtenemos el número %d\n\n",
+          caracter1, caracter1, caracter2, caracter2, caracter1 + caracter2);
+   
+  return 0;
+}
+
+/********************************************/
+
+
 
 /*********************************************/
 
-/*************** Ejercicio 2.7 ***************/
+/*************** Ejercicio 3.2 ***************/
 
 /*********************************************/  
 
-/*************** Ejercicio 2.8 ***************/
+/*************** Ejercicio 3.3 ***************/
 
 /*********************************************/  
 
