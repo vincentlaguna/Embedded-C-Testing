ba8eaf2a40fc2f4758fa5dab794fff3b5b2d8778
vincentlaguna <vincentlaguna@live.com>
2021-07-12 19:27:38 +0000

    UAMx ProgC01 - EDx Course - 3 Tablas y Cadenas - Introduccion a las Tablas - cometida #0 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -666,7 +666,7 @@ int main(void)
 /*********************************************/
 
 
-/*********************************************/  
+/*********************************************
 // Tablas y Cadenas - Macros y Constantes - Macros con Parametros
 #include <stdio.h>
 
@@ -750,7 +750,7 @@ int main(void)
 
 int main(void) 
 {
-  int datos_a[DIM] = {8,7,9};   // 3 elementos
+  int datos_a[DIM] = {8,7.5,9};   // 3 elementos
   int datos_b[] = {8,7,9};      // 3 elementos
   int datos_c[DIM+2] = {8,7,9}; // 5 elementos
   int datos_d[DIM+22] = {0};    // 25 elementos
