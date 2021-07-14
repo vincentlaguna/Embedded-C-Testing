b3ec46ae031b9e4a5768ba0de371e450d36fbd62
vincentlaguna <vincentlaguna@live.com>
2021-07-14 21:14:53 +0000

    UAMx ProgC01 - EDx Course - 3 Tablas y Cadenas - Tablas Bidimensionales - cometida #0 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -798,7 +798,7 @@ int main(void)
 /*********************************************/  
 
 
-/*************** Ejercicio 3.3 ***************/
+/*************** Ejercicio 3.3 ***************
 // Vamos a escribir un programa que calcule la media de experiencia obtenida
 // en las últimas 5 partidas al LoL con 3 de tus campeones más habituales.
 
@@ -864,8 +864,23 @@ int main(void)
 }
 
 /*********************************************/  
+// Introduccion a las Tablas Bidimensionales
 
+#include <stdio.h>
 
+#define DIM 2
+
+int main (void) 
+{
+  int num[DIM][DIM] = {{1, 3}, {5, 3}};
+  int deter = 0;
+   
+  deter = num[0][0] * num[1][1] - num[1][0] * num[0][1];
+
+  printf ("El determinante es: %d ", deter);
+
+  return 0;
+}
 
 /*********************************************/  
 
