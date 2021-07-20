e3384aa0e1509595dc6ed2a5b47e5cf547ec0ad4
vincentlaguna <vincentlaguna@live.com>
2021-07-20 21:43:19 +0000

    UAMx ProgC01 - EDx Course - 3 Tablas y Cadenas - Cadenas de caracteres (strings) - Analizando strcmp() - cometida #0 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -1177,7 +1177,7 @@ int main(void)
 /*********************************************/
 
 
-/*********************************************/
+/*********************************************
 // Analizando gets()
 
 #include <stdio.h>
@@ -1206,6 +1206,30 @@ int main(void)
 }
 
 /*********************************************/
+
+
+/*********************************************/
+// Analizando strcmp()
+
+#include <stdio.h>
+#include <string.h>
+
+#define DIM 8
+
+int main(void)
+{
+  char nombre1[DIM] = "Ana";
+  char nombre2[DIM] = "Anabel";
+
+  printf("\nAna vs. Anabel: %d\n", strcmp(nombre1, nombre2));
+  printf("Ana vs. Ana: %d\n", strcmp(nombre1, nombre1));
+  printf("Anabel vs. Ana: %d\n\n", strcmp(nombre2, nombre1));
+  
+  return(0);
+}
+
+/*********************************************/
+
 
 /*************** Ejercicio 3.8 ***************/
 
