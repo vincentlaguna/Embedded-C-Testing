d1b83b780ce6527eda108a6bc697c662c69aef49
vincentlaguna <vincentlaguna@live.com>
2021-07-24 00:13:17 +0000

    Reading and setting Bits practice - commit #0 - C

--- sandbox/DoodleBox/src/Read_Set_Bits.c
+++ sandbox/DoodleBox/src/Read_Set_Bits.c
@@ -4,8 +4,19 @@
 
 int main(int argc, char **argv)
 {
-  puts("\n  ***** Hello World! ***** \n");
-  puts("<--- Reading and Setting Bits ---> \n\n");
+  puts("\n\t     ***** Hello World! *****\n");
+  puts("\t<--- Reading and Setting Bits --->\n\n");
+  
+  int num, position, newNum, bitStatus;
+  // Get Input from User
+  printf("Enter any number: ");
+  scanf("%d", &num);
+  // Enter the bit position you want to set...
+  printf("Enter the nth bit position to check and set (0-31): ");
+  scanf("%d", &position); 
+  // Right-shift num (n) number of times and perform bitwise & with 1
+  bitStatus = (num >> position) & 1;
+  puts("The %d bit is set to: %d\n", position, bitStatus);
   
   return(0);
 }
