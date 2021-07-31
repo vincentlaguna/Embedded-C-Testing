e51623d5d73f54d906216d24bd2ae658b20fce1c
vincentlaguna <vincentlaguna@live.com>
2021-07-31 18:06:54 +0000

    UAMx ProgC01 - EDx Course - 4.0 Estructuras - Estructuras Anidadas - cometida #1 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -1719,9 +1719,43 @@ int main(void)
 // Estructuras Anidadas
 #include  <stdio.h>
 
+#define MAX_STR 128
+
+typedef struct {
+    char nombre[MAX_STR];
+    char primer_apellido[MAX_STR];
+    char segundo_apellido[MAX_STR];
+} Escritor;
+
+typedef struct {
+    Escritor autor;
+    char titulo[MAX_STR];
+    int anyo;
+} Libro;
+
 int main(void)
 {
- 
+  Libro novela;
+
+  printf("Introduce el nombre del autor: ");
+  gets(novela.autor.nombre);
+    
+  printf("Introduce el primer apellido del autor: ");
+  gets(novela.autor.primer_apellido);
+  
+  printf("Introduce el segundo apellido del autor: ");
+  gets(novela.autor.segundo_apellido);
+  
+  printf("Introduce el título del libro: ");
+  gets(novela.titulo);
+  
+  printf("Introduce el año de publicación del libro: ");
+  scanf("%d", &novela.anyo);
+
+  printf("%s fue escrito en %d por %s %s %s.\n",
+          novela.titulo, novela.anyo, novela.autor.nombre,
+          novela.autor.primer_apellido, novela.autor.segundo_apellido);
+          
   return(0);
 }
 
@@ -1752,3 +1786,5 @@ int main(void)
 }
 
 /*********************************************/
+
+v
