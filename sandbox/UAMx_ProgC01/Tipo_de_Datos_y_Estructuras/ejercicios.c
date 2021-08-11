228776e28fc5279cb5f3a48926aaed60bedfa1da
vincentlaguna <vincentlaguna@live.com>
2021-08-11 03:45:51 +0000

    UAMx ProgC01 - EDx Course - 4.0 Estructuras - Tablas de Estructuras - Ejercicio 4.5 - cometida #7 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -2219,11 +2219,39 @@ int main(void)
   static double  mediaTotal;
   _Invocador     invocador[JUGADORES];
   
-  for (int i = 0; i < PARTIDAS; i++)
+  for (int i = 0; i < JUGADORES; i++)
   {
     printf("\nDATOS JUGADOR %d\n", i+1);
     printf("\n¿Cuál es tu nombre de invocador?: ");
-    scanf("%s", invocador[i].nombre);
+    scanf("%s", invocador[i].partida[].nombre);
+  
+    printf("¿Con qué campeón has jugado tu última partida?: ");
+    scanf("%s", invocador[i].partida[].ultCampeon);
+  
+    printf("¿Cuántos asesinatos has hecho?: ");
+    scanf("%d", &invocador[i].partida[].kda[asesinatos]);
+  
+    printf("¿Cuántas veces has muerto?: ");
+    scanf("%d", &invocador[i].partida[].kda[muertes]);
+  
+    printf("¿Y cuántas asistencias has hecho?: ");
+    scanf("%d", &invocador[i].partida[].kda[assists]);
+    
+    // KDA Ratio = (K+A) / Max(1,D)
+    if (!invocador[i].juego.kda[muertes] >= 1) { invocador[i].juego.kda[muertes] = 1; }
+  
+    invocador[i].juego.ratioKDA = (invocador[i].juego.kda[asesinatos] + 
+    invocador[i].juego.kda[assists]) / (double)invocador[i].juego.kda[muertes];
+    
+    mediaUsuarios[asesinatos] += invocador[i].juego.kda[asesinatos];
+    mediaUsuarios[muertes]    += invocador[i].juego.kda[muertes];
+    mediaUsuarios[assists]    += invocador[i].juego.kda[assists];
+    
+    for (int i = 0; i < JUGADORES; i++)
+  {
+    printf("\nDATOS JUGADOR %d\n", i+1);
+    printf("\n¿Cuál es tu nombre de invocador?: ");
+    scanf("%s", invocador[i].partida[].nombre);
   
     printf("¿Con qué campeón has jugado tu última partida?: ");
     scanf("%s", invocador[i].partida[].ultCampeon);
