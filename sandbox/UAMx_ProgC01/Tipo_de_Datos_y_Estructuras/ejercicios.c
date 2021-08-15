38d6bb81ba19ff203259589b375df810073715ed
vincentlaguna <vincentlaguna@live.com>
2021-08-15 19:52:56 +0000

    UAMx ProgC01 - EDx Course - 4.0 Estructuras - Prueba de Evaluacion 4.1 - cometida #0 - C

--- sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
+++ sandbox/UAMx_ProgC01/Tipo_de_Datos_y_Estructuras/ejercicios.c
@@ -2121,7 +2121,7 @@ int main(void)
 /*********************************************/
 
 
-/*************** Ejercicio 4.5 ***************/
+/*************** Ejercicio 4.5 ***************
 // Vamos a hacer un último programa, el más complejo de los 
 // que hemos hecho hasta ahora, en el que queremos trabajar 
 // con tablas de estructuras que contienen como atributo 
@@ -2271,6 +2271,65 @@ int main(void)
   
   return(0);
   
+}
+
+/*********************************************/
+
+
+/**************** Prueba 4.1 *****************/
+Escribe un programa en C que:
+
+Declare una estructura llamada Suma con una tabla de tres enteros
+(utiliza una macro para definir su tamaño) y un entero para almacenar
+la suma de los elementos de la tabla.
+
+Declare otra estructura llamada Resto con una tabla de dos elementos 
+de tipo Suma (utiliza una macro para definir su tamaño) y un entero 
+para almacenar el módulo de la división de las dos sumas.
+
+El programa declarará una variable estructurada 
+a partir de la estructura Resto.
+
+A continuación solicitará los tres enteros de la primera tabla
+y los guardará en los miembros correspondientes de la variable estructurada.
+
+A continuación calculará la suma de los tres números leídos
+y los guardará en el miembro correspondiente de la variable estructurada.
+
+A continuación solicitará los tres enteros de la segunda tabla
+y los guardará en los miembros correspondientes de la variable estructurada.
+
+A continuación calculará la suma de los tres números leídos
+y los guardará en el miembro correspondiente de la variable estructurada.
+
+A continuación calculará el resto de la división de las dos sumas
+y lo guardará en el miembro correspondiente de la variable estructurada.
+
+Por último mostrará la información de las sumas y restos por pantalla
+de la forma que se especifica en el ejemplo más abajo.
+
+Los mensajes se han de mostrar tal y como aparecen en 
+el siguiente ejemplo de ejecución y en el mismo orden 
+(variando sólo según los datos introducidos por el usuario).
+
+Lo que sigue es un ejemplo de ejecución del programa:
+
+Introduce el primer entero de la primera tabla: 14
+Introduce el segundo entero de la primera tabla: 19
+Introduce el tercer entero de la primera tabla: 8
+Introduce el primer entero de la segunda tabla: 4
+Introduce el segundo entero de la segunda tabla: 11
+Introduce el tercer entero de la segunda tabla: 3
+La suma de los elementos de la primera tabla es 41.
+La suma de los elementos de la segnda tabla es 18.
+El resto de la división de ambas sumas es 5.
+
+#include  <stdio.h>
+
+int main(void)
+{
+ 
+  return(0);
 }
 
 /*********************************************/
