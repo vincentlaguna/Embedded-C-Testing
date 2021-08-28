c1595676442f677595a595a471364d2ad9ee965c
vincentlaguna <vincentlaguna@live.com>
2021-08-28 20:16:24 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - Operadores relacionales y logicos - Cometida Inicial del Curso #0 - C

---
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -0,0 +1,31 @@
+// UAMx ProgC02
+
+// Introducción a la programación en C: 
+// Instrucciones de control y ficheros de texto
+
+/*********************************************
+
+#include  <stdio.h>
+
+int main(void)
+{
+ 
+  return(0);
+}
+
+/*********************************************/
+
+
+/*************** Ejercicio 0.0 ***************
+
+#include  <stdio.h>
+
+int main(void)
+{
+ 
+  return(0);
+}
+
+/*********************************************/
+
+
