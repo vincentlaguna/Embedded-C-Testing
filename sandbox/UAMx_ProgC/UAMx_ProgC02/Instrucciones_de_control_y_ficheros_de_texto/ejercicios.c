bdc4d69c8420bd20b1604bdba96460413855e8a7
vincentlaguna <vincentlaguna@live.com>
2021-08-28 22:56:45 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - Operadores relacionales y logicos - AND y OR - cometida #0 - C

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -3,6 +3,31 @@
 // Introducción a la programación en C: 
 // Instrucciones de control y ficheros de texto
 
+/*********************************************/
+// Operadores Logicos AND y OR
+
+#include  <stdio.h>
+
+int main(void)
+{
+  int x=3;
+  
+  printf("Recuerda: la operación devuelve 1 si se cumple (true) y 0 si no se cumple (false)\n");
+  
+  printf("El resultado de evaluar x>2 && x<=5 es: %d\n", x>2 && x<=5);
+
+  printf("El resultado de evaluar x!=2 && x>4 es: %d\n", x!=2 && x>4);
+   
+  printf("El resultado de evaluar x>=2 || x>5 es: %d\n", x>=2 || x>5);   
+   
+  printf("El resultado de evaluar x>3 || x==2 es: %d\n", x>3 || x==2);
+ 
+  return(0);
+}
+
+/*********************************************/
+
+
 /*********************************************
 
 #include  <stdio.h>
