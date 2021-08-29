5c5146e210c87bd1fb2a9eef12f61f05a0c278ed
vincentlaguna <vincentlaguna@live.com>
2021-08-29 19:47:01 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - Operadores relacionales y logicos - Ejercicio 1.1 - cometida #1 - C

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -54,19 +54,20 @@ int main(void)
 /*********************************************/
 
 
-/*************** Ejercicio 1.1 ***************
-Escribe un programa que solicite tu edad y muestre 
-un 1 por pantalla si eres mayor de edad (18 años o más)
-o tu edad no es múltiplo de tres. En caso contrario,
-el programa mostrará un 0.
+/*************** Ejercicio 1.1 ***************/
+// Escribe un programa que solicite tu edad y muestre 
+// un 1 por pantalla si eres mayor de edad (18 años o más)
+// o tu edad no es múltiplo de tres. En caso contrario,
+// el programa mostrará un 0.
 
-Prueba qué se muestra si introduces los valores 15, 17, 20 y 21.
+// Prueba qué se muestra si introduces los valores 15, 17, 20 y 21.
 
 #include  <stdio.h>
 
 int main(void)
 {
- 
+  printf("\nQue edad tienes")
+  scanf()
   return(0);
 }
 
