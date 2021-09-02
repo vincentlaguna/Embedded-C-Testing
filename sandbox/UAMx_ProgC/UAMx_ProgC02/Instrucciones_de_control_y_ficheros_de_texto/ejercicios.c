c9fa8e05313af15a685bd49dfb51c78464180234
vincentlaguna <vincentlaguna@live.com>
2021-09-02 19:49:36 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - Condicionales - if/else - Ejercicio 1.3 - cometida #1 - C

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -113,7 +113,29 @@ int main(void)
 
 
 /*************** Ejercicio 1.3 ***************
+Escribe un programa que compruebe si eres menor de edad y, 
+si es cierto, muestre el mensaje "Eres menor de edad".
 
+En caso de que seas mayor de edad, comprobará si tu edad 
+es múltiplo de tres y mostrará el mensaje 
+"Eres mayor de edad y tu edad es múltiplo de tres" o 
+"Eres mayor de edad y tu edad no es múltiplo de tres" 
+dependiendo de ello.
+
+Este sería un ejemplo del programa, escribiendo 15 como edad:
+
+Introduce tu edad: 15
+Eres menor de edad
+
+Otro ejemplo, escribiendo como edad 31:
+
+Introduce tu edad: 31
+Eres mayor de edad y tu edad no es múltiplo de tres
+
+Y otro, escribiendo 21:
+
+Introduce tu edad: 21
+Eres mayor de edad y tu edad es múltiplo de tres
 #include  <stdio.h>
 
 int main(void)
