d680fbd4ebbe9e6463b1151551ef85bd3dda0dd1
vincentlaguna <vincentlaguna@live.com>
2021-09-04 20:14:32 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - Condicionales - La instruccion switch y las enumeraciones - cometida #0 - C

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -252,7 +252,7 @@ int main(void)
 /*********************************************/
 
 
-/*************** Ejercicio 1.5 ***************/
+/*************** Ejercicio 1.5 ***************
 // Vamos a escribir un programa un poco más complejo,
 // que lea del teclado dos fechas en forma de día,
 // mes y año separados por espacios en blanco.
@@ -352,6 +352,43 @@ int main(void)
     printf("\nLa entrade fue erronea.\n\n");
   }
   
+  return(0);
+}
+
+/*********************************************/
+
+
+/*********************************************/
+// La instruccion switch y las enumeraciones
+
+#include  <stdio.h>
+
+int main(void)
+{
+  int opcion;
+
+  printf("\nMenú:\n  1. Empezar partida\n  2. Ajustes\n  3. Ayuda\n  4. Salir\n");
+  printf("Selecciona una opción: "); 
+  scanf("%d", &opcion);
+
+  switch (opcion)
+  {
+    case 1:          
+      printf("Has elegido comenzar la partida.\n");
+      break;
+    case 2:          
+      printf("Aquí estará el código para mostrar los ajustes del juego.\n");
+      break;
+    case 3:          
+      printf("Aquí estará el código para mostrar la ayuda.\n");
+      break;
+    case 4:          
+      printf("¡Hasta luego!\n");
+      break;
+    default:
+      printf("Opción incorrecta.\n");
+    }
+    
   return(0);
 }
 
