e04e519714665cccbcf04ab852eab92b644873fa
vincentlaguna <vincentlaguna@live.com>
2021-09-06 23:56:38 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - 2. El bucle while y do-while: do-while - Ejercicio 2.6 - cometida #1 - C

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -1272,38 +1272,88 @@ int main(void)
 
 
 /*************** Ejercicio 2.6 ***************/
-Vamos a escribir un programa bucles while y/o do-while,
-según consideres. Este programa tiene que solicite al 
-usuario cuántos números quiere leer por teclado (entre 2 y 10),
-leerlos y mostrar aquellos que sean menores al último introducido.
+// Vamos a escribir un programa bucles while y/o do-while,
+// según consideres. Este programa tiene que solicite al 
+// usuario cuántos números quiere leer por teclado (entre 2 y 10),
+// leerlos y mostrar aquellos que sean menores al último introducido.
 
-Recuerda comprobar que el valor introducido por el usuario es correcto.
+// Recuerda comprobar que el valor introducido por el usuario es correcto.
 
-Esto sería un ejemplo del programa:
+// Esto sería un ejemplo del programa:
 
-¿Cuántos números quieres comparar? Elige un número entre 2 y 10: 11
-El número introducido no es correcto, vuelve a intentarlo: 4
-Introduce el número 1 de 4: 3
-Introduce el número 2 de 4: 7
-Introduce el número 3 de 4: 5
-Introduce el número 4 de 4: 6
-El número 5.00 es menor que el número 6.00
-El número 3.00 es menor que el número 6.00
+// ¿Cuántos números quieres comparar? Elige un número entre 2 y 10: 11
+// El número introducido no es correcto, vuelve a intentarlo: 4
+// Introduce el número 1 de 4: 3
+// Introduce el número 2 de 4: 7
+// Introduce el número 3 de 4: 5
+// Introduce el número 4 de 4: 6
+// El número 5.00 es menor que el número 6.00
+// El número 3.00 es menor que el número 6.00
 
-Y este, otro:
+// Y este, otro:
 
-¿Cuántos números quieres comparar? Elige un número entre 2 y 10: 5
-Introduce el número 1 de 5: 5
-Introduce el número 2 de 5: 4
-Introduce el número 3 de 5: 3
-Introduce el número 4 de 5: 2
-Introduce el número 5 de 5: 1
+// ¿Cuántos números quieres comparar? Elige un número entre 2 y 10: 5
+// Introduce el número 1 de 5: 5
+// Introduce el número 2 de 5: 4
+// Introduce el número 3 de 5: 3
+// Introduce el número 4 de 5: 2
+// Introduce el número 5 de 5: 1
 
 #include  <stdio.h>
+#define   DIM 10
 
 int main(void)
 {
- 
+  int i = 0;
+  int numero;
+  int tabla[DIM];
+  
+  printf("\n¿Cuántos números quieres comparar? Elige un número entre 2 y 10: ");
+  scanf("%d", &numero);
+  
+  do
+  {
+    if (numero < 2 || numero > DIM)
+    {
+      printf("\nEl número introducido no es correcto, vuelve a intentarlo: ");
+      scanf("%d", &numero);
+    }
+    
+  } while (numero < 2 || numero > DIM);
+  
+  while (i < numero)
+  {
+    printf("\nIntroduce el número %d de %d: ", i+1, numero);
+    scanf("%d", &tabla[i]);
+    i++;
+  }
+  
+  i = 0;
+  
+  while (i < numero)
+  {
+    if (tabla[i] < tabla[numero-1])
+    {
+      printf("\nEl número %d es menor que el número %d",
+              tabla[i], tabla[numero-1]);
+      i++;
+    }
+    else
+    {
+      i++;
+    }
+  }
+  // for (int x = 0; x < numero; x++)
+  // {
+  //   if (tabla[x] < tabla[numero])
+  //   {
+  //     printf("El número %d es menor que el número %d",
+  //             tabla[x], tabla[numero]);
+  //   }
+  // }
+  
+  printf("\n\n");
+  
   return(0);
 }
 
