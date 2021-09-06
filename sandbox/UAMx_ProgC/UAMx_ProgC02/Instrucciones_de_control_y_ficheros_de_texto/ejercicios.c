e24a244aa7bb5be96524b18e39345356b586bdc3
vincentlaguna <vincentlaguna@live.com>
2021-09-06 23:17:19 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - 2. El bucle while y do-while: do-while - Ejercicio 2.6 - cometida #0 - C

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -1209,7 +1209,7 @@ int main(void)
 /*********************************************/
 
 
-/*************** Ejercicio 2.5 ***************/
+/*************** Ejercicio 2.5 ***************
 // Vamos a modificar ahora el ejercicio 2.3 para que el 
 // programa compruebe que el número introducido está
 // comprendido entre 1 y 10. En caso contrario, 
@@ -1242,7 +1242,7 @@ int main(void)
   int i = 0;
   
   printf("\n¿Cuántos elementos quieres mostrar por pantalla? \n"
-        "Elige un número entre 1 y 10: ");
+         "Elige un número entre 1 y 10: ");
   scanf("%d", &numero);
   
   printf("\n");
@@ -1255,14 +1255,55 @@ int main(void)
       scanf("%d", &numero);
     }
     
-    printf("%d ", tabla[i]);
-    i++;
-  
-    
-  } while (i < numero && !(numero < 0 || numero > DIM));
+    while (i < numero)
+    {
+      printf("%d ", tabla[i]);
+      i++;
+    }
+      
+  } while (numero < 0 || numero > DIM);
   
   printf("\n\n");
   
+  return(0);
+}
+
+/*********************************************/
+
+
+/*************** Ejercicio 2.6 ***************/
+Vamos a escribir un programa bucles while y/o do-while,
+según consideres. Este programa tiene que solicite al 
+usuario cuántos números quiere leer por teclado (entre 2 y 10),
+leerlos y mostrar aquellos que sean menores al último introducido.
+
+Recuerda comprobar que el valor introducido por el usuario es correcto.
+
+Esto sería un ejemplo del programa:
+
+¿Cuántos números quieres comparar? Elige un número entre 2 y 10: 11
+El número introducido no es correcto, vuelve a intentarlo: 4
+Introduce el número 1 de 4: 3
+Introduce el número 2 de 4: 7
+Introduce el número 3 de 4: 5
+Introduce el número 4 de 4: 6
+El número 5.00 es menor que el número 6.00
+El número 3.00 es menor que el número 6.00
+
+Y este, otro:
+
+¿Cuántos números quieres comparar? Elige un número entre 2 y 10: 5
+Introduce el número 1 de 5: 5
+Introduce el número 2 de 5: 4
+Introduce el número 3 de 5: 3
+Introduce el número 4 de 5: 2
+Introduce el número 5 de 5: 1
+
+#include  <stdio.h>
+
+int main(void)
+{
+ 
   return(0);
 }
 
