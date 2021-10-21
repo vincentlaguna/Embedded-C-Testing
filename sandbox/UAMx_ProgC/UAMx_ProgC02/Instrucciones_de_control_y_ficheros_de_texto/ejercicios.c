8a0f9c7c437e7f11b518cceaa07cec613d206b79
vincentlaguna <vincentlaguna@live.com>
2021-10-21 13:38:10 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - 3. El Bucle For - Ejercicio 3.1 - cometida #1

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -1645,38 +1645,38 @@ int main(void)
 
 
 /*************** Ejercicio 3.1 ***************/
-Haz un programa que muestre la tabla de multiplicar 
-de un número (entre 1 y 9) introducido por el usuario.
+// Haz un programa que muestre la tabla de multiplicar 
+// de un número (entre 1 y 9) introducido por el usuario.
 
-Esto sería un ejemplo del programa:
+// Esto sería un ejemplo del programa:
 
-Introduce un número entre 1 y 9: 5
-La tabla del 5 es:
-5 por 1 es 5
-5 por 2 es 10
-5 por 3 es 15
-5 por 4 es 20
-5 por 5 es 25
-5 por 6 es 30
-5 por 7 es 35
-5 por 8 es 40
-5 por 9 es 45
-5 por 10 es 50
+// Introduce un número entre 1 y 9: 5
+// La tabla del 5 es:
+// 5 por 1 es 5
+// 5 por 2 es 10
+// 5 por 3 es 15
+// 5 por 4 es 20
+// 5 por 5 es 25
+// 5 por 6 es 30
+// 5 por 7 es 35
+// 5 por 8 es 40
+// 5 por 9 es 45
+// 5 por 10 es 50
 
-Y este, otro:
+// Y este, otro:
 
-Introduce un número entre 1 y 9: 9
-La tabla del 9 es:
-9 por 1 es 9
-9 por 2 es 18
-9 por 3 es 27
-9 por 4 es 36
-9 por 5 es 45
-9 por 6 es 54
-9 por 7 es 63
-9 por 8 es 72
-9 por 9 es 81
-9 por 10 es 90
+// Introduce un número entre 1 y 9: 9
+// La tabla del 9 es:
+// 9 por 1 es 9
+// 9 por 2 es 18
+// 9 por 3 es 27
+// 9 por 4 es 36
+// 9 por 5 es 45
+// 9 por 6 es 54
+// 9 por 7 es 63
+// 9 por 8 es 72
+// 9 por 9 es 81
+// 9 por 10 es 90
 
 
 #include  <stdio.h>
@@ -1691,12 +1691,14 @@ int main(void)
   
   printf("\nIntroduce un número entre 1 y 9: ");
   scanf("%d", &numero);
+  printf("\nLa tabla del %d es:\n", numero);
   
-  for (i=0; i < DIM; i++);
+  
+  for (i = 0; i < DIM; i++)
   {
-    printf("%d\n", i);
+    printf("%d por %d es: %d\n", numero, i, numero * i);
   }
-
+  putchar('\n');
   
   return(0);
 }
