bef8664c2feab8479dcd0adf64f4df172b94e450
vincentlaguna <vincentlaguna@live.com>
2021-10-21 03:28:58 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - 3. El Bucle For - Ejercicio 3.1 - cometida #0

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -1644,18 +1644,55 @@ int main(void)
 /*********************************************/
 
 
-/*************** Ejercicio 0.0 ***************/
+/*************** Ejercicio 3.1 ***************/
+Haz un programa que muestre la tabla de multiplicar 
+de un número (entre 1 y 9) introducido por el usuario.
+
+Esto sería un ejemplo del programa:
+
+Introduce un número entre 1 y 9: 5
+La tabla del 5 es:
+5 por 1 es 5
+5 por 2 es 10
+5 por 3 es 15
+5 por 4 es 20
+5 por 5 es 25
+5 por 6 es 30
+5 por 7 es 35
+5 por 8 es 40
+5 por 9 es 45
+5 por 10 es 50
+
+Y este, otro:
+
+Introduce un número entre 1 y 9: 9
+La tabla del 9 es:
+9 por 1 es 9
+9 por 2 es 18
+9 por 3 es 27
+9 por 4 es 36
+9 por 5 es 45
+9 por 6 es 54
+9 por 7 es 63
+9 por 8 es 72
+9 por 9 es 81
+9 por 10 es 90
+
 
 #include  <stdio.h>
 
-#define   DIM 3
+#define   DIM 10
 
 int main(void)
 {
   
   int i;
-
-  for (i=0; i<DIM; i++);
+  int numero;
+  
+  printf("\nIntroduce un número entre 1 y 9: ");
+  scanf("%d", &numero);
+  
+  for (i=0; i < DIM; i++);
   {
     printf("%d\n", i);
   }
