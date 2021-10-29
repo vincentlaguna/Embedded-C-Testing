145ff348e19c2691f875837fbb091fc72ecf9a75
vincentlaguna <vincentlaguna@live.com>
2021-10-29 23:16:01 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - 3. El Bucle For y Las Tablas - Ejercicio 3.6 - cometida #0

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -1988,7 +1988,7 @@ int main(void)
 /*********************************************/
 
 
-/*************** Ejercicio 3.5 ***************/
+/*************** Ejercicio 3.5 ***************
 // Si alguna vez te has preguntado cómo se 
 // representan los números decimales en binario, 
 // en este ejercicio vamos a crear un programa que 
@@ -2072,6 +2072,64 @@ int main(void)
     printf("%d", resultado[j]);
     
   printf("\n\n");
+  
+  return(0);
+}
+
+/*********************************************/
+
+
+/*************** Ejercicio 3.6 ***************/
+// Utiliza bucles anidados para imprimir las tablas
+// de multiplicar de los números del 1 al 9.
+
+// Esta sería la salida del programa:
+
+// La tabla del 1 es:
+// 1 por 1 es 1
+// 1 por 2 es 2
+// 1 por 3 es 3
+// 1 por 4 es 4
+// 1 por 5 es 5
+// 1 por 6 es 6
+// 1 por 7 es 7
+// 1 por 8 es 8
+// 1 por 9 es 9
+// 1 por 10 es 10
+
+// [Resto de tablas...]
+
+// La tabla del 9 es:
+// 9 por 1 es 9
+// 9 por 2 es 18
+// 9 por 3 es 27
+// 9 por 4 es 36
+// 9 por 5 es 45
+// 9 por 6 es 54
+// 9 por 7 es 63
+// 9 por 8 es 72
+// 9 por 9 es 81
+// 9 por 10 es 90
+
+#include  <stdio.h>
+
+#define   DIM 10
+
+int main(void)
+{
+  int i, j;
+  
+  for (i = 1; i < DIM; i++)
+  {
+    printf("\nLa tabla del %d es: \n", i);
+    
+    for (j = 1; j <= DIM; j++)
+    {
+      printf("%d por %d es %d\n", i, j, i * j);
+    }
+  }
+  
+  putchar('\n');
   
   return(0);
 }
