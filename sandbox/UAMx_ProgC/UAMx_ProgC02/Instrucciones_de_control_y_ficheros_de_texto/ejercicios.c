1d4f73b701e94c820e9f515dddaa8da10afd595e
vincentlaguna <vincentlaguna@live.com>
2021-11-03 20:46:59 +0000

    UAMx ProgC02 - EDx Course - Instrucciones de control y ficheros de texto - 3. El Bucle For y Las Tablas - Prueba de Evaluacion 2.2 - cometida #6

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -2288,34 +2288,34 @@ int main(void)
 
 
 /**************** Prueba 2.2 *****************/
-Escribe un programa en C que solicite 
-exactamente por teclado tantos números como 
-se especifiquen en una macro 
-(del tamaño que consideres) y guárdalos en 
-una tabla del tamaño de la macro.
+// Escribe un programa en C que solicite 
+// exactamente por teclado tantos números como 
+// se especifiquen en una macro 
+// (del tamaño que consideres) y guárdalos en 
+// una tabla del tamaño de la macro.
 
-A continuación, por cada número introducido,
-el programa mostrará cuántos de los números 
-introducidos después son mayores que él.
+// A continuación, por cada número introducido,
+// el programa mostrará cuántos de los números 
+// introducidos después son mayores que él.
 
-El programa ha de ser genérico para cualquier 
-valor de la macro. Si por ejemplo, 
-se cambiara el valor a 7, funcionaría 
-exactamente igual con siete números sin hacer 
-ninguna otra modificación en el código.
+// El programa ha de ser genérico para cualquier 
+// valor de la macro. Si por ejemplo, 
+// se cambiara el valor a 7, funcionaría 
+// exactamente igual con siete números sin hacer 
+// ninguna otra modificación en el código.
 
-Lo que sigue es un ejemplo de ejecución
-con una macro definida con 5:
+// Lo que sigue es un ejemplo de ejecución
+// con una macro definida con 5:
 
-Introduce el número 1: 2
-Introduce el número 2: 7
-Introduce el número 3: 1
-Introduce el número 4: 4
-Introduce el número 5: 5
-El número 2 es menor que otros 3 números posteriores.
-El número 7 es menor que otros 0 números posteriores.
-El número 1 es menor que otros 2 números posteriores.
-El número 4 es menor que otros 1 números posteriores.
+// Introduce el número 1: 2
+// Introduce el número 2: 7
+// Introduce el número 3: 1
+// Introduce el número 4: 4
+// Introduce el número 5: 5
+// El número 2 es menor que otros 3 números posteriores.
+// El número 7 es menor que otros 0 números posteriores.
+// El número 1 es menor que otros 2 números posteriores.
+// El número 4 es menor que otros 1 números posteriores.
 
 #include  <stdio.h>
 #define   DIM 5
@@ -2328,20 +2328,30 @@ int main(void)
   printf("\n");
   for (i = 0; i < DIM; i++)
   {
-    printf("Introduce el número %d: ");
+    printf("Introduce el número %d: ", i+1);
     scanf("%d", &numeros[i]);
     
-    for (j = 0; j < DIM; j++)
+    if (numeros[i] < numeros[--i])
     {
-      if (numeros[i] > numeros)  
+      colocados[i] = numeros[i];
+    }
+    else
+    {
+      colocados[++i] = numeros[i];
     }
   }
-  for (j = 0; j < DIM; j++)
+  
+  for (i = 0; i < DIM; i++)
   {
-    printf("El número %d es menor que otros %d números posteriores.\n",
-            numeros[i]);
+    printf("Numeros[%d] <---> Colocados[%d]\n", numeros[i], colocados[i]);
     
   }
+  
+  // for (j = 0; j < DIM; j++)
+  // {
+  //   printf("El número %d es menor que otros %d números posteriores.\n",
+  //           numeros[i]);
+  // }
   
   printf("\n");
   return(0);
