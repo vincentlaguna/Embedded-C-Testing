ae2ea5287615714d83a429877c946b43cc0a26b6
vincentlaguna <vincentlaguna@live.com>
2021-11-12 23:43:01 +0000

    UAMx ProgC03 - EDx Course - Funciones y Punteros - 1. Funciones y sus Argumentsos - cometida #1

--- sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
@@ -3,16 +3,52 @@
 // Introducción a la programación en C: 
 // Funciones y Punteros
 
-/*********************************************
-// Operadores Logicos AND y OR
+/*********************************************/
 
 #include  <stdio.h>
 
+void menu(void)
+{
+  puts("\n");
+  
+  printf("Menú:\n\n  1. Empezar partida\n  2. Ajustes\n  3. Ayuda\n  4. Salir\n\n");
+  printf("Selecciona una opción: ");
+  
+  return;
+}
+
 int main(void)
 {
+  int opcion;
   
+  do
+  {
+    menu();
+    scanf("%d", &opcion);
+    
+  } while (opcion < 1 || opcion > 4);
   
- 
+  switch (opcion)
+  {
+    case 1: 
+      printf("Has elegido comenzar la partida.\n");
+      break;
+    
+    case 2: 
+      printf("Aquí estará el código para mostrar los ajustes del juego.\n");
+      break;
+    
+    case 3:
+      printf("Aquí estará el código para mostrar la ayuda.\n");
+      break;
+      
+    case 4:
+      printf("¡Hasta luego!\n");
+      break;
+  }
+  
+  puts("\n");
+  
   return(0);
 }
 
