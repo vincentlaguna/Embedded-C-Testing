b84c240646ab400d9d6143e9a7e6b6cacbae13d2
vincentlaguna <vincentlaguna@live.com>
2021-11-13 00:13:04 +0000

    UAMx ProgC03 - EDx Course - Funciones y Punteros - 1. Funciones y sus Argumentsos - cometida #2

--- sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
@@ -3,7 +3,7 @@
 // Introducción a la programación en C: 
 // Funciones y Punteros
 
-/*********************************************/
+/*********************************************
 
 #include  <stdio.h>
 
@@ -49,6 +49,48 @@ int main(void)
   
   puts("\n");
   
+  return(0);
+}
+
+/*********************************************/
+
+
+/*************** Ejercicio 1.1 ***************/
+Escribe un programa con una función que 
+solicite la base y la altura de un triángulo
+y muestre por pantalla su área.
+
+La función main se encargará únicamente de 
+llamar a esta función, y no declarará ninguna
+variable ni tendrá ninguna otra instrucción.
+
+Este sería un ejemplo del programa:
+
+Introduce la base del triángulo: 12
+Introduce la altura del triángulo: 7
+El área del triángulo de base
+12 y altura 7 es 42.00.
+
+Y otro:
+
+Introduce la base del triángulo: 37
+Introduce la altura del triángulo: 10
+El área del triángulo de base
+37 y altura 10 es 185.00.
+
+#include  <stdio.h>
+
+int main(void)
+{
+  int base;
+  int altura;
+  double area;
+  
+  printf("\nIntroduce la base del triángulo: ");
+  scanf("%d", &base);
+  printf("\nIntroduce la altura del triángulo: ");
+  printf("El área del triángulo de base %d y altura %d es %lf\n",
+          );
   return(0);
 }
 
