a09074fbdaeb9b1ca95a7ebd3598bb66d9a08a10
vincentlaguna <vincentlaguna@live.com>
2021-11-18 21:16:44 +0000

    UAMx ProgC03 - EDx Course - Funciones y Punteros - 1. Funciones con Retorno - Ejercicio 1.6 - cometida #17

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -2443,7 +2443,7 @@ int main(void)
   printf("len: %d, string: %s\n", tam, cadena);
   char concuerda[tam];// = {'\0'};
   // char *fijos[tam];// = {'\0'};
-  char *fijos[tam];// = malloc(tam * sizeof(char));
+  char fijos[tam][TAMV];// = malloc(tam * sizeof(char));
   putchar('\n');
   
   for (i = 0; i < tam; ++i)
@@ -2454,7 +2454,7 @@ int main(void)
       if (cadena[i] == vocales[j])
       {
         concuerda[j] = vocales[j];
-        *(*fijos + i) = vocales[j]; // This is the debug point
+        fijos[i][j] = vocales[j]; // This is the debug point
         // printf("concuerda[%d] = %c ", x, concuerda[x]);
       }
       else
