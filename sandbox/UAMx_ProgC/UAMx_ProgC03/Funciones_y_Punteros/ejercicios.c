f7ba4065b15efbc8d3b959e5b9675be0285c5f7b
vincentlaguna <vincentlaguna@live.com>
2021-11-25 21:50:17 +0000

    UAMx ProgC03 - EDx Course - Funciones y Punteros - Prueba 1.1 - cometida #0

--- sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
@@ -464,7 +464,7 @@ int main()
 /*********************************************/
 
 
-/*************** Ejercicio 1.6 ***************/
+/*************** Ejercicio 1.6 ***************
 // Escribe una función con el siguiente prototipo:
 // int primoMayor(int num). La función recibe un 
 // número en la variable num y ha de devolver el 
@@ -562,6 +562,96 @@ int main()
   
   puts("\n");
   
+  return(0);
+}
+
+/*********************************************/
+
+
+/**************** Prueba 1.1 *****************/
+Codifica una función de tipo entero llamada 
+diferencia() que reciba dos argumentos de 
+tipo char y compruebe que están entre la 
+A y la Z o entre la a y la z.
+
+La función ha de devolver la diferencia en
+caracteres que hay entre las dos letras, 
+sin tener en cuenta minúsculas y mayúsculas
+y siempre retornando un número positivo.
+Si no lo están devolverá -1. Por ejemplo:
+
+Para los caracteres & y a devolverá -1.
+Para los caracteres a y b devolverá 1.
+Para los caracteres A y b devolverá 1.
+Para los caracteres b y a devolverá 1.
+Para los caracteres D y b devolverá 2.
+
+Escribe un programa principal que solicite 
+dos caracteres y llame a la función diferencia.
+Dependiendo del retorno de la función
+podrá mostrar tres mensajes posibles:
+
+Un mensaje de error informando de que alguno 
+de los caracteres introducidos no es una letra.
+Un mensaje informando de que la diferencia 
+entre ambas letras es de un carácter.
+Un mensaje informando de que la diferencia 
+entre ambas letras es de x caracteres (para x>1).
+Si se ha mostrado el mensaje de error, 
+el programa debe volver a solicitar las 
+letras, llamar a la función y 
+mostrar el mensaje hasta que se
+introduzcan valores válidos.
+
+Lo que sigue es un ejemplo
+de ejecución del programa:
+
+Introduce una letra de del abecedario en 
+mayúsculas o minúsculas (excepto la ñ): d
+
+Introduce otra letra de del abecedario en
+mayúsculas o minúsculas (excepto la ñ): +
+
+Error, solo puedes introducir letras del abecedario.
+Introduce una letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): -
+Introduce otra letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): R
+Error, solo puedes introducir letras del abecedario.
+Introduce una letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): F
+Introduce otra letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): s
+La diferencia entre la F y la s es de 13 caracteres.
+
+Esto es otro ejemplo:
+
+Introduce una letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): t
+Introduce otra letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): m
+La diferencia entre la t y la m es de 7 caracteres.
+
+Otro:
+
+Introduce una letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): T
+Introduce otra letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): m
+La diferencia entre la T y la m es de 7 caracteres.
+
+Otro:
+
+Introduce una letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): a
+Introduce otra letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): A
+La diferencia entre la a y la A es de 0 caracteres.
+
+Y otro:
+
+Introduce una letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): c
+Introduce otra letra de del abecedario en mayúsculas o minúsculas (excepto la ñ): D
+La diferencia entre la c y la D es de un carácter.
+
+NOTA1: Fíjate que este último mensaje es diferente a los anteriores, al haber solo un carácter de diferencia.
+
+NOTA2: La función no debe solicitar ningún valor por teclado ni mostrar ningún mensaje por pantalla.
+#include  <stdio.h>
+
+int main(void)
+{
+ 
   return(0);
 }
 
