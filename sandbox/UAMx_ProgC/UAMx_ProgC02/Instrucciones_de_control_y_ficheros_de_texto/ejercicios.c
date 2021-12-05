f233d87b73358e2fb2ab5d14962c5e18e0a081b5
vincentlaguna <vincentlaguna@live.com>
2021-12-05 22:23:03 +0000

    UAMx ProgC02 - EDx Course - Prueba 3.2 - nuevas cometidas #22

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -2374,7 +2374,7 @@ int main(void)
 /*********************************************/
 
 
-/**************** Prueba 3.2 *****************/
+/**************** Prueba 3.2 *****************
 // Escribe un programa en C que solicite por 
 // teclado una cadena de caracteres y guárdala en
 // una variable del tamaño 128 (utiliza macros).
@@ -2528,6 +2528,119 @@ for (int x = 0; x < TAM; x++)
   
   // putchar('\n'); 
   
+  return(0);
+}
+
+/*********************************************/
+
+
+/**************** Prueba 3.2 *****************/
+// Escribe un programa en C que solicite por 
+// teclado una cadena de caracteres y guárdala en
+// una variable del tamaño 128 (utiliza macros).
+
+// Para simplificar, puedes asumir que el usuario va
+// a introducir el carácter '_' en lugar de espacios.
+
+// A continuación, para cada carácter de la cadena, 
+// el programa mostrará por pantalla todas las 
+// vocales minúsculas anteriores a dicho carácter.
+
+// Lo que sigue es un ejemplo de ejecución:
+
+// Escribe una cadena de caracteres: Hola_mundo
+// las vocales anteriores al caracter H son:
+// las vocales anteriores al caracter o son:
+// las vocales anteriores al caracter l son: o
+// las vocales anteriores al caracter a son: o
+// las vocales anteriores al caracter _ son: o a
+// las vocales anteriores al caracter m son: o a
+// las vocales anteriores al caracter u son: o a
+// las vocales anteriores al caracter n son: o a u
+// las vocales anteriores al caracter d son: o a u
+// las vocales anteriores al caracter o son: o a u
+
+// Otro ejemplo:
+
+// Escribe una cadena de caracteres: Yo_soy_tu_padre
+// Las vocales anteriores al caracter Y son:                          
+// Las vocales anteriores al caracter o son:                          
+// Las vocales anteriores al caracter _ son: o                        
+// Las vocales anteriores al caracter s son: o                        
+// Las vocales anteriores al caracter o son: o                        
+// Las vocales anteriores al caracter y son: o o                      
+// Las vocales anteriores al caracter _ son: o o                      
+// Las vocales anteriores al caracter t son: o o                      
+// Las vocales anteriores al caracter u son: o o                      
+// Las vocales anteriores al caracter _ son: o o u                    
+// Las vocales anteriores al caracter p son: o o u                    
+// Las vocales anteriores al caracter a son: o o u                    
+// Las vocales anteriores al caracter d son: o o u a                  
+// Las vocales anteriores al caracter r son: o o u a                  
+// Las vocales anteriores al caracter e son: o o u a
+
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <string.h>
+
+#define   TAM  128
+#define   TAMV 6
+
+int main(void)
+{
+  int i, j, x, y;
+  int tam;
+  char cadena[TAM] = {'\0'};
+  char vocales[] = {' ', 'a', 'e', 'i', 'o', 'u'};
+  
+  printf("\nEscribe una cadena de caracteres: ");
+  scanf("%s", cadena);
+  tam = strlen(cadena);
+  printf("len: %d, string: %s\n", tam, cadena);
+
+  putchar('\n');
+
+  for (i = 1; i < tam; ++i)
+  {
+    printf("Las vocales anteriores al caracter %c son: ", cadena[i]);
+    for (j = 1; j <= TAMV; ++j)
+    {
+      if (cadena[i] == vocales[j])
+      {
+        printf("%c", cadena[i]);
+      }
+    }
+    putchar('\n');
+  }
+  
+  putchar('\n'); 
+  
+  return(0);
+}
+
+/*********************************************/
+
+
+/*************** Ejercicio 0.0 ***************
+
+#include  <stdio.h>
+
+int main(void)
+{
+ 
+  return(0);
+}
+
+/*********************************************/
+
+
+/*********************************************
+
+#include  <stdio.h>
+
+int main(void)
+{
+ 
   return(0);
 }
 
