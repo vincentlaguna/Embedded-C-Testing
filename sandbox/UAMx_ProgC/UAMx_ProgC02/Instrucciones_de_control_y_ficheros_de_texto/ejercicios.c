3c4365a9bae2f07f396c1afe7d6bbfca6c10bccf
vincentlaguna <vincentlaguna@live.com>
2021-12-16 18:15:40 -0500

    UAMx ProgC02 - EDx Course - Prueba 3.2 - nuevas cometidas #39

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -2613,7 +2613,7 @@ int main(void)
         {
           if (i == contador)
           {
-            // printf("%c\n", cadena[contador]);
+            printf("%c\n", cadena[contador]);
           }
           // printf("%c%c", cadena[i-contador], cadena[i-1]);
           // printf("%c", cadena[i-1]);
