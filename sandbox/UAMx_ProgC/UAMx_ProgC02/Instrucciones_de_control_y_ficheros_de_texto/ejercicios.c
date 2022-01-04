0aaa0a871b1eed52c179225084b46744a88f0eef
vincentlaguna <vincentlaguna@live.com>
2022-01-03 20:09:08 -0500

    UAMx ProgC02 - EDx Course - 4. Archivos de Texto - Ejercicio 4.4 - cometida #1

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -2855,50 +2855,50 @@ int main(void)
 
 
 /*************** Ejercicio 4.4 ***************/
-Partiendo de un fichero compuesto por un 
-número indeterminado de filas de 3 columnas, 
-cada una de ellas formada por
- una cadena y 2 enteros:
+// Partiendo de un fichero compuesto por un 
+// número indeterminado de filas de 3 columnas, 
+// cada una de ellas formada por
+// una cadena y 2 enteros:
 
-Elena 25 43
-Juan 5 8
-Sonia 2 1
-Julia 3 17
+// Elena 25 43
+// Juan 5 8
+// Sonia 2 1
+// Julia 3 17
 
-Escribe un programa que solicite el nombre 
-del archivo con dichos datos y una letra 
-inicial. A continuación, ha de leer los datos
-del fichero (teniendo en cuenta que el fichero 
-puede tener cualquier número de líneas) 
-y escribir al final del mismo cuántos nombres 
-empiezan por la letra solicitada. 
-El programa acabará informando si el resultado 
-se ha escrito correctamente en el fichero 
-o si este no se ha podido abrir.
+// Escribe un programa que solicite el nombre 
+// del archivo con dichos datos y una letra 
+// inicial. A continuación, ha de leer los datos
+// del fichero (teniendo en cuenta que el fichero 
+// puede tener cualquier número de líneas) 
+// y escribir al final del mismo cuántos nombres 
+// empiezan por la letra solicitada. 
+// El programa acabará informando si el resultado 
+// se ha escrito correctamente en el fichero 
+// o si este no se ha podido abrir.
 
-Este es un ejemplo de ejecución basado en los 
-datos anteriores (Elena, Juan, Sonia, Julia), 
-que hemos guardado en datos.txt:
+// Este es un ejemplo de ejecución basado en los 
+// datos anteriores (Elena, Juan, Sonia, Julia), 
+// que hemos guardado en datos.txt:
 
-Teclea el nombre del archivo: datas.txt
-Teclea una inicial: J
-Error: no se pudo abrir el fichero datas.txt.
+// Teclea el nombre del archivo: datas.txt
+// Teclea una inicial: J
+// Error: no se pudo abrir el fichero datas.txt.
 
-Y otro:
+// Y otro:
 
-Teclea el nombre del archivo: datos.txt
-Teclea una inicial: J
-El resultado se ha guardado correctamente 
-al final del fichero datos.txt
+// Teclea el nombre del archivo: datos.txt
+// Teclea una inicial: J
+// El resultado se ha guardado correctamente 
+// al final del fichero datos.txt
 
-En este segundo ejemplo, el fichero datos.txt 
-habrá quedado de la forma:
+// En este segundo ejemplo, el fichero datos.txt 
+// habrá quedado de la forma:
 
-Elena 25 43
-Juan 5 8
-Sonia 2 1
-Julia 3 17
-Hay 2 nombres que empiezan por la inicial J.
+// Elena 25 43
+// Juan 5 8
+// Sonia 2 1
+// Julia 3 17
+// Hay 2 nombres que empiezan por la inicial J.
 
 #include  <stdio.h>
 
