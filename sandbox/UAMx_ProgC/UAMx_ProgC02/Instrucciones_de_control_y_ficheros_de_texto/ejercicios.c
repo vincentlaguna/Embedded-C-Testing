33f1fb48029fb5309ba023cc5a164c8f964d1d01
vincentlaguna <vincentlaguna@live.com>
2022-01-24 18:12:25 -0500

    school work Prueba 4.1 - 01242022

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -2854,7 +2854,7 @@ int main(void)
 /*********************************************/
 
 
-/*************** Ejercicio 4.4 ***************/
+/*************** Ejercicio 4.4 ***************
 // Partiendo de un fichero compuesto por un 
 // número indeterminado de filas de 3 columnas, 
 // cada una de ellas formada por
@@ -2974,6 +2974,137 @@ int main(void)
 /*********************************************/
 
 
+/**************** Prueba 4.1 *****************/
+// Partiendo de un fichero compuesto por un número
+// indeterminado de filas de 3 columnas, 
+// cada una de ellas formada por una cadena 
+// y 2 enteros, como el que sigue:
+
+// Elena 5 3
+// Juan 24 8
+// Sonia 12 1
+
+// Haz un programa principal que solicite el 
+// nombre del archivo con los datos de entrada, 
+// otro nombre para el archivo que se va a crear 
+// con los datos de salida y un número entero.
+
+// El programa debe crear un fichero de texto con
+// los nombres de las personas cuya suma de la 
+// segunda y tercera columnas se encuentra por 
+// debajo del valor númerico introducido.
+
+// El programa acabará mostrando un mensaje 
+// indicando si se ha podido guardar el resultado o 
+// si ha habido algún problema al abrir los ficheros.
+
+// Lo que sigue son varios ejemplos de ejecución 
+// (para estos ejemplos el nombre correcto del 
+// archivo de entrada es datos.txt):
+
+// Teclea el nombre del archivo de entrada: datis.txt
+// Teclea el nombre del archivo de salida: nombres.txt
+// Teclea el valor del límite: 25
+// Error: no se pudo abrir el fichero de entrada.
+
+// Lo que sigue es otro ejemplo de ejecución:
+
+// Teclea el nombre del archivo de entrada: datos.txt
+// Teclea el nombre del archivo de salida: nombres.txt
+// Teclea el valor del límite: 25
+// Se ha creado correctamente el fichero nombres.txt 
+// con los resultados
+
+// Para este últiimo caso, y basándose en el fichero 
+// puesto como ejemplo al principio del enunciado, 
+// nombres.txt contendrá los siguientes datos:
+
+// Elena
+// Sonia
+
+// El programa ha de funcionar igual con cualquier 
+// fichero que pueda tener cualquier número de líneas.
+
+// NOTA: Si abres el fichero resultante desde el Bloc 
+// de notas de Windows es probable que veas toda la 
+// información en la misma línea. Para poder verlo 
+// correctamente, utiliza otro editor de 
+// texto como WordPad.
+
+#include  <stdio.h>
+#include  <string.h>
+
+#define   TAM        256
+#define   TAM_CAD    32
+
+
+typedef struct 
+{
+  // Datos
+  char    nombre[TAM_CAD];
+  int     num;
+  int     num1;
+
+} Notas;
+
+int main(void)
+{
+  FILE    *fFicha;
+  int     i;
+  static int contador;    
+  char    inicial;
+  static  Notas notas[TAM];
+  char    ficha[TAM_CAD];
+
+  printf("\nTeclea el nombre del archivo: ");
+  gets(ficha);
+
+  if ((fFicha = fopen(ficha, "r")) == NULL)
+  {
+    printf("Error: no se pudo abrir el fichero %s.\n", ficha);
+    return (-1);
+  }
+
+  printf("Teclea una inicial: ");
+  scanf("%c", &inicial);
+  
+
+ for (i = 0; fscanf(fFicha, "%s", notas[i].nombre) == 1; i++)    
+  {
+    fscanf(fFicha, "%d", &notas[i].num);
+    fscanf(fFicha, "%d", &notas[i].num1);
+  }
+
+  if ((fFicha = fopen(ficha, "a")) == NULL)
+  {
+    printf("Error: no se pudo abrir el fichero %s.\n", ficha);
+    return (-1);
+  }
+
+  while (i > 0)
+  {
+
+    if (notas[i].nombre[0] == inicial)
+    {
+      // fprintf(fFicha, "%c ", notas[i].nombre[0]);
+      contador++;
+      i--;
+    }
+    i--;
+  }
+  
+  fprintf(fFicha, "Hay %d nombres que empiezan por la inicial %c.\n", contador, inicial);
+
+  printf("El resultado se ha guardado correctamente al final del fichero %s\n", ficha);
+  
+  fclose(fFicha);
+
+  return(0);
+}
+
+/*********************************************/
+
+ 
 /*************** Ejercicio 0.0 ***************
 
 #include  <stdio.h>
