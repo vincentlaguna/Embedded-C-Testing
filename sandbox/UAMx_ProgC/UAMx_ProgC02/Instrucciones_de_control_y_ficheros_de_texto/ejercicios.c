0781fe33bde7e370853ab826061852670468c901
vincentlaguna <vincentlaguna@live.com>
2022-01-29 18:49:26 -0500

    school work Prueba 4.2 - cometida #0 - 01292022

--- sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC02/Instrucciones_de_control_y_ficheros_de_texto/ejercicios.c
@@ -2974,7 +2974,7 @@ int main(void)
 /*********************************************/
 
 
-/**************** Prueba 4.1 *****************/
+/**************** Prueba 4.1 *****************
 // Partiendo de un fichero compuesto por un número
 // indeterminado de filas de 3 columnas, 
 // cada una de ellas formada por una cadena 
@@ -3077,6 +3077,123 @@ int main(void)
   } 
 
   printf("\nTeclea el valor del límite: ");
+  scanf("%d", &limite);
+  
+
+ for (i = 0; fscanf(fFichaE, "%s", notas[i].nombre) == 1; i++)    
+  {
+    fscanf(fFichaE, "%d", &notas[i].num);
+    fscanf(fFichaE, "%d", &notas[i].num1);
+
+    if (((notas[i].num) + (notas[i].num1)) < limite)
+    {
+      fprintf(fFichaS, "%s\n", notas[i].nombre);
+    }
+  }
+
+  printf("\nSe ha creado correctamente el fichero nombres.txt con los resultados\n");
+  
+  fclose(fFichaE);
+  fclose(fFichaS);
+
+  return(0);
+}
+
+/*********************************************/
+
+
+/**************** Prueba 4.2 *****************/
+// Partiendo de un fichero compuesto por un 
+// número indeterminado de filas de 5 columnas, 
+// cada una de ellas formada por 2 enteros, 
+// seguidos de un número real y otros dos enteros, 
+// como el que sigue:
+
+// 25 43 3.4 5 8
+// 12 3 -1.2 12 3
+// 2 7 1.3 8 4
+
+// El programa ha de solicitar el nombre del 
+// archivo con los datos, así como un valor entero. 
+// A continuación debe calcular la media de los 
+// valores de la segunda columna para los que la 
+// cuarta columna es mayor que el entero
+// introducido por teclado.
+
+// Por ejemplo, si se introduce el valor 5,
+// se calculará (3 + 7) / 2, es decir, 5.
+
+// Finalmente, debe mostrar el resultado del cálculo.
+
+// Lo que sigue son varios ejemplos de ejecución
+// (para estos ejemplos, el nombre correcto
+// del archivo es datos.txt):
+
+// Teclea el nombre del archivo: datos.txt
+// Teclea un número entero: 5
+// La media de la segunda columna con 
+// el límite igual a 5 es: 5.00
+
+// Lo que sigue es otro ejemplo de ejecución:
+
+// Teclea el nombre del archivo: datos.txt
+// Teclea un número entero: 12
+// La media de la segunda columna con 
+// el límite igual a 12 es: 0.00
+
+// Lo que sigue es otro ejemplo de ejecución:
+
+// Teclea el nombre del archivo: datas.txt
+// Teclea un número entero: 12
+// Error: no se pudo abrir el fichero datas.txt
+
+
+#include  <stdio.h>
+#include  <string.h>
+
+#define   TAM        256
+#define   TAM_CAD    32
+
+
+typedef struct 
+{
+  // Datos
+  char    nombre[TAM_CAD];
+  int     num;
+  int     num1;
+
+} Notas;
+
+int main(void)
+{
+  FILE        *fFichaE;
+  FILE        *fFichaS;
+  int         i;   
+  char        fichaE[TAM_CAD];
+  char        fichaS[TAM_CAD];
+  static      Notas notas[TAM];
+  static int  limite;
+  static int  contador;
+
+  printf("\nTeclea el nombre del archivo: ");
+  gets(fichaE);
+
+  if ((fFichaE = fopen(fichaE, "r")) == NULL)
+  {
+    printf("Error: no se pudo abrir el fichero %s.\n", fichaE);
+    return (-1);
+  }
+
+  printf("\nTeclea el nombre del archivo de salida: ");
+  gets(fichaS);
+
+  if ((fFichaS = fopen(fichaS, "w")) == NULL)
+  {
+    printf("Error: no se pudo abrir el fichero %s.\n", fichaS);
+    return (-1);
+  } 
+
+  printf("\nTeclea el valor del lÃ­mite: ");
   scanf("%d", &limite);
   
 
