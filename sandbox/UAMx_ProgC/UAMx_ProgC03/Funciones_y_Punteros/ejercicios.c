8ad556dbbe024c7430e31857619465fb87b0b82c
vincentlaguna <vincentlaguna@live.com>
2022-02-06 18:09:52 -0500

    school work Module 3 - Ejercicio 2.1 - cometida #0 - 02062022

--- sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
@@ -759,6 +759,76 @@ int main(void)
 
 
 /*********************************************
+// Practica de punteros
+#include  <stdio.h>
+
+int main(void)
+{
+  // int i = 10, j, *p;
+  int i, *p;
+  // j = ;
+  // p = i;
+  // i = 5;
+  i = 1000000;
+
+  printf("\nLa variable i vale = %d\n\n", i);    
+
+  p  = &i;
+  *p = 10;
+
+  printf("\nLa variable i vale = %d\n\n", i);
+  // printf("\ni = %d, j = %d, *p = %d\n\n", i, j, *p);
+  return(0);
+}
+
+/*********************************************/
+
+
+/*************** Ejercicio 2.1 ***************/
+// Escribe un programa que calcule la media de 
+// dos enteros introducidos por teclado y la 
+// muestre por pantalla, pero utilizando 
+// punteros en vez de variables.
+
+// Obligatoriamente tienes que utilizar dos
+// variables enteras y sus correspondientes
+// punteros, así como otra variable de tipo
+// double para la media, y su 
+// correspondiente puntero.
+
+// Desde el momento de la asignación, 
+// únicamente puede trabajarse con punteros.
+
+// Recomendación: si crees que te va a resultar
+// difícil trabajar directamente con los
+// punteros, puedes hacer el programa con 
+// variables normales y después sustituir 
+// según lo que te hemos explicado.
+
+// Este sería un ejemplo del programa:
+
+// Introduce un número: 4
+// Introduce otro número: 13
+// media = 8.50
+
+// Y otro:
+
+// Introduce un número: 345
+// Introduce otro número: 76
+// media = 210.50
+
+#include  <stdio.h>
+
+int main(void)
+{
+ 
+  return(0);
+}
+
+/*********************************************/
+
+
+/*********************************************
 
 #include  <stdio.h>
 
@@ -797,9 +867,8 @@ int main(void)
 /*********************************************/
 
 
-/*********************************************/
+/*********************************************
 // Simple Hash Map example
-
 #include  <stdio.h>
 #include  <stdlib.h>
 #include  <stdint.h>
