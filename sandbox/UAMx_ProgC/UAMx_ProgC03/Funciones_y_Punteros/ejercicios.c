78a6994a68d8d310f63ea8f9fb0eef80175afd9e
vincentlaguna <vincentlaguna@live.com>
2022-03-08 17:21:22 -0500

    UAMx School work - Paso de argumentos de tipo estructura - Prueba 2.1 - commit #1 - 03082022

--- sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
@@ -1198,6 +1198,58 @@ int main(void)
 
 
 /**************** Prueba 2.1 *****************/
+Escribe una función que calcule el menor y el
+mayor de los divisores de un número. 
+Este sería su prototipo:
+
+int menorMayorDivisor(int num, int *menor, int *mayor);
+
+Los posibles divisores no tendrán en cuenta 
+el 1 ni el propio número. Si el número no 
+tiene divisores, tanto en menor como en mayor
+almacenarán el número 0. Asimismo, la función
+ha de devolver 0 si el número recibido es
+inferior a 2, y por lo tanto no se pueden 
+calcular sus divisores. En caso contrario, 
+acabará devolviendo 1.
+
+El programa principal solicitará un número y, 
+según los valores obtenidos, realizará una de 
+las siguientes operaciones:
+
+Informe de que el número introducido es 
+incorrecto y vuelva a solicitarlo.
+
+Informe de que el número no tiene divisores.
+Informe del divisor menor y mayor.
+
+Lo que sigue es un ejemplo de ejecución del programa:
+
+Introduce un número: 1
+Error. Debes introducir un número mayor o igual a 2.
+Introduce un número: -4
+Error. Debes introducir un número mayor o igual a 2.
+Introduce un número: 10
+El menor divisor de 10 es 2 y su mayor divisor es 5 
+(sin contar la unidad y él mismo).
+
+Otro ejemplo:
+
+Introduce un número: 155
+El menor divisor de 155 es 5 y su mayor divisor es 31 
+(sin contar la unidad y él mismo).
+
+Otro:
+
+Introduce un número: 11
+El número 11 no tiene divisores menores ni mayores
+(sin contar la unidad y él mismo).
+
+Y otro:
+
+Introduce un número: 9
+El menor divisor de 9 es 3 y su mayor divisor es 3 
+(sin contar la unidad y él mismo).
 
 #include  <stdio.h>
 
