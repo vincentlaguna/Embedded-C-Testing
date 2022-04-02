cee096bafd8b30ae375677d9c6c9ddd8f14dab84
vincentlaguna <vincentlaguna@live.com>
2022-04-02 18:17:39 -0400

    UAMx School work - Paso de argumentos de tipo estructura - Prueba 2.2 - commit #0 - 04022022

--- sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
@@ -1197,7 +1197,7 @@ int main(void)
 /*********************************************/
 
 
-/**************** Prueba 2.1 *****************/
+/**************** Prueba 2.1 *****************
 // Escribe una función que calcule el menor y el
 // mayor de los divisores de un número. 
 // Este sería su prototipo:
@@ -1328,7 +1328,61 @@ int main()
 /*********************************************/
 
 
-/*************** Ejercicio 0.0 ***************
+/**************** Prueba 2.2 *****************/
+Escribe una función con el siguiente prototipo:
+
+int cifras(int num, int * primera, int * ultima);
+
+La función recibe un número entero en la variable
+num y ha de calcular los siguientes valores:
+
+La primera cifra del número, que guardará 
+en primera.
+La última cifra del número, que guardrá 
+en ultima.
+La cantidad de cifras del número, que retornará 
+al finalizar.
+
+Por ejemplo, para el número 468 la primera cifra
+es el 4, la última el 8 y la cantidad de cifras 3.
+
+NOTA: La función ha de trabajar siempre con el
+número en formato entero, sin considerarlo o 
+convertirlo a caracteres o cadena de caracteres
+y sin utilizar funciones como itoa o sprintf.
+
+Escribe un programa que solicite el número por
+teclado, llame a la función y muestre el 
+resultado obtenido por pantalla. Los mensajes 
+se han de mostrar tal y como aparecen en el 
+siguiente ejemplo de ejecución y en el mismo 
+orden (variando solo según los números 
+introducidos por el usuario).
+ 
+Lo que sigue es un ejemplo de 
+ejecución del programa:
+
+Introduce un número entero: 578913
+El número 578913 tiene 6 cifras. Su primera 
+cifra es el 5 y su última cifra es el 3.
+
+Otro ejemplo:
+
+Introduce un número entero: 75
+El número 75 tiene 2 cifras. Su primera 
+cifra es el 7 y su última cifra es el 5.
+
+Otro:
+
+Introduce un número entero: -289
+El número -289 tiene 3 cifras. Su primera 
+cifra es el 2 y su última cifra es el 9.
+
+Y otro:
+
+Introduce un número entero: -4
+El número -4 tiene 1 cifras. Su primera 
+cifra es el 4 y su última cifra es el 4.
 
 #include  <stdio.h>
 
