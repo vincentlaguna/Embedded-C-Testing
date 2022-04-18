0df5072d2e29a4152904463402bb0fb1a740fe13
vincentlaguna <vincentlaguna@live.com>
2022-04-18 00:27:54 -0400

    UAMx School work - Paso de argumentos de tipo estructura - Prueba 2.2 - nada mas consigue la ultima cifra por ahora... - commit #36 -  04172022

--- sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
@@ -1393,7 +1393,8 @@ int cifras(unsigned int num, int *primera, int *ultima)
   int temp = 0;
   
   ult = num % 10;
-  // res = log10(num);
+  res = log10(num);
+  printf("\nlog10(num): %d\n", res);
   // prim = num / pow(10, res);
   temp = num;
   // temp = temp / 10;
