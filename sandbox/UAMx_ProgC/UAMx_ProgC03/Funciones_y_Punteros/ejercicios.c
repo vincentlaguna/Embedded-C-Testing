212466b14702f6bf5be7590059d6100190feb262
vincentlaguna <vincentlaguna@live.com>
2022-04-29 15:13:49 -0400

    UAMx School work - Punteros y Tablas - Paso de tablas como argumentos de funciones - commit #0 -  04292022

--- sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
@@ -1599,6 +1599,36 @@ int main(void)
 /*********************************************/
 
 
+/*********************************************/
+
+#include <stdio.h>
+
+#define DIM 3
+
+void incrementa(int *p, int num) 
+{
+  int i;
+  
+  for (i=0; i<num; i++)
+    p[i]++;
+}
+
+int main() 
+{
+  int i, datos[DIM] = {2,3,5};
+
+  incrementa(datos, DIM);
+
+  for (i=0; i<DIM; i++)
+    printf("%d ", datos[i]);
+
+  return 0;
+
+}
+
+/*********************************************/
+
+
 /*********************************************
 
 #include  <stdio.h>
