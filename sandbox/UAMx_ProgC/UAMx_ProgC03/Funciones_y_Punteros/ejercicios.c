d58f9a3ae94ab56336d1c482f502092441dc0985
vincentlaguna <vincentlaguna@live.com>
2022-06-01 04:39:16 +0000

    Reserva dinamica de memoria de tablas bi-dimensionales (matrices) - Cometida #0 - 05312022

--- sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
+++ sandbox/UAMx_ProgC/UAMx_ProgC03/Funciones_y_Punteros/ejercicios.c
@@ -2110,7 +2110,7 @@ int main(void)
 /*********************************************/
 
 
-/*************** Ejercicio 4.2 ***************/
+/*************** Ejercicio 4.2 ***************
 // Escribe un programa que reserve memoria 
 // dinámica para una tabla de 
 // elementos de tipo Fecha:
@@ -2285,6 +2285,51 @@ int main() {
 
     return 0;
 */
+/*********************************************/
+
+
+/*********************************************/
+// Reserva dinámica de memoria de tablas bidimensionales (matrices)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i, j, estudiantes, examenes;
+    double **notas;
+
+    printf("¿Cuántos estudiantes hay en tu clase? ");
+    scanf("%d", &estudiantes);
+    printf("¿Cuántos exámenes habéis hecho? ");
+    scanf("%d", &examenes);
+
+    notas = (double**) malloc(examenes*sizeof(double*));
+    if (notas == NULL) {
+        printf("La reserva de memoria ha fallado.");
+        return 1;
+    }
+
+    for (i=0; i<examenes; i++) {
+        notas[i] = (double*) malloc(estudiantes*sizeof(double));
+        
+        if (notas[i] == NULL) {
+            for (j=0; j<i; j++)
+                free(notas[j]);
+            free(notas);            
+            printf("La reserva de memoria ha fallado.");
+            return 1;
+        }  
+    }
+
+    // Código para almacenar las notas de los estudiantes para cada examen.
+
+    for (i=0; i<examenes; i++)
+        free(notas[i]);
+    free(notas);
+
+    return 0;
+
+
 /*********************************************/
 
 
